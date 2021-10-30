9a19b6a7a910dcca9bbd32c1de8a01d3f7a74d41
SimonB87 <buryans@gmail.com>
2021-10-30 12:25:11 +0200

    edited setdate.js - adding zero for date-parts below 10

--- shared/assets/js/setdate.js
+++ shared/assets/js/setdate.js
@@ -1,11 +1,38 @@
 const elDate = document.getElementById("registrationdate");
 
-const currentdate = new Date(); 
-const datetime = "Datum: " + currentdate.getDate() + ". "
-                + (currentdate.getMonth()+1)  + ". " 
-                + currentdate.getFullYear() + " - "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+const currentDate = new Date(); 
 
-elDate.value = datetime;
+let stringDate = currentDate.getDate();
+stringDate = editStringDateValue(stringDate);
+
+let stringMonth = (currentDate.getMonth()+1);
+stringMonth = editStringDateValue(stringMonth);
+
+const stringYear = currentDate.getFullYear();
+
+let stringHour = currentDate.getHours();
+stringHour = editStringDateValue(stringHour);
+
+let stringMinutes = currentDate.getMinutes();
+stringMinutes = editStringDateValue(stringMinutes);
+
+let stringSeconds = currentDate.getSeconds();
+stringSeconds = editStringDateValue(stringSeconds);
+
+const dateTime = "Datum: " + stringDate + ". "
+                + stringMonth + ". " 
+                + stringYear + " - "  
+                + stringHour + ":"  
+                + stringMinutes + ":" 
+                + stringSeconds;
+
+elDate.value = dateTime;
+
+function editStringDateValue(inputValue) {
+  if ((inputValue > 0) && (inputValue < 10)) {
+    const editedValue = "0" + inputValue;
+    return editedValue;
+  } else {
+    return inputValue;
+  }
+}
