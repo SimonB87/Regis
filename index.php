9df848fd494d7952a4b955f94834d440fd932ac0
SimonB87 <buryans@gmail.com>
2021-10-30 12:40:09 +0200

    edited indentation redability

--- index.php
+++ index.php
@@ -11,33 +11,33 @@
   <head>
   <body>
 
-  <?php
-  include("shared/navigation.php");
-  ?> 
+    <?php
+    include("shared/navigation.php");
+    ?> 
 
-  <?php
-  include("webform/herosection.php");
-  ?> 
+    <?php
+    include("webform/herosection.php");
+    ?> 
 
-  <?php
-  include("webform/description.php");
-  ?> 
+    <?php
+    include("webform/description.php");
+    ?> 
 
-  <?php
-  include("webform/registrationform.php");
-  ?> 
+    <?php
+    include("webform/registrationform.php");
+    ?> 
 
-  <?php
-  include("shared/footer.php");
-  ?>
+    <?php
+    include("shared/footer.php");
+    ?>
 
-  <script src="shared/libs/bootstrap/js/bootstrap.js"></script>
+    <script src="shared/libs/bootstrap/js/bootstrap.js"></script>
 
-<!--
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-  <script src="shared/assets/js/formvalidation.js"></script>
-  <script src="shared/assets/js/recalculateprice.js"></script>
-  <script src="shared/assets/js/setdate.js"></script>
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+    <script src="shared/assets/js/formvalidation.js"></script>
+    <script src="shared/assets/js/recalculateprice.js"></script>
+    <script src="shared/assets/js/setdate.js"></script>
 
   </body>
 </html>
