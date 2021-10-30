43f17f22da24bcb474cc2e21ca40d655529ab4a9
SimonB87 <buryans@gmail.com>
2021-10-30 19:42:32 +0200

    added quick contact section

---
+++ shared/contactsection.php
@@ -0,0 +1,17 @@
+<div class="bg-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2>Event organizer</h2>
+          <p>Mauritius & Elvira Bachata Prague <p>
+        </div>
+        <div class="col-6">
+          <p><strong>Email : </strong> mauritius.elvira[at]gmail.com  <p>
+        </div>
+        <div class="col-6">
+          <p><strong>Phone, WhatsApp, Telegram : </strong> + 420 777 216 875  <p>
+        </div>
+
+      </div>
+    </div>
+  </div>
