7f2d5959042d4fdba0d84100fd32afee1ec1b3aa
SimonB87 <buryans@gmail.com>
2021-10-30 19:42:44 +0200

    edited emai texts

--- manage/emailing/sendmaintoclient.php
+++ manage/emailing/sendmaintoclient.php
@@ -1,43 +1,51 @@
 <?php
 
-  $to				= 	"buryans@gmail.com";
-  $subject	=		"Testovací prostředí - Máme novou registraci order-ID: \"" . $orderId . "\" na akci \"" . $eventName . "\" | Platforma REGIS";
-  
+if ($correctlyFilledForm) {
+
+  $to				= 	$clientEmail;
+  $subject	=		"Event \"" . $eventName . "\" registration order-ID: \"" . $orderId . "\" and payment instructions";
+
   $message_head	=	"<div style='max-width: 550px; margin: 0 auto; line-height: 1.5rem; font-size: 0.9rem;'>" .
-                  "<p>Krásný den, </p><p>na testovací prostředí přístála další <b>nová registrace do platformy REGIS</b></p></div><br>";
-  
-  
-  
+                  "<p>Thank you for registering for then event " . $eventName  . ", </p><p>please send the required amount by a bank transfer <b>according to instructions bellow</b></p></div><br>";
+
+
+
   $message_body = "<div style='max-width: 550px; margin: 0 auto; line-height: 1.5rem; font-size: 0.9rem; padding: 30px 30px 30px 30px;'> <div>" .
+
                   "<h2 style='padding: 0; margin: 0; Margin: 0;text-align: left; line-height: 1.3; color: inherit; word-wrap: normal; font-family: Helvetica, Arial, sans-serif; margin-bottom: 10px; Margin-bottom: 10px; border-bottom: 3px solid #ffc600; padding-bottom: 10px;'>" .
-                  "Detaily nové registrace" .
+                  "Payment instructions" .
+                  "</h2>".
+
+                  "<div>" . $userNotification . "</div>" .
+
+                  "<h2 style='padding: 0; margin: 0; Margin: 0;text-align: left; line-height: 1.3; color: inherit; word-wrap: normal; font-family: Helvetica, Arial, sans-serif; margin-bottom: 10px; Margin-bottom: 10px; border-bottom: 3px solid #ffc600; padding-bottom: 10px;'>" .
+                  "Registration details" .
                   "</h2>".
                   "<div> <h3> Order - ID:</h3><p>" . $orderId . "</p> </div>" .
                   $htmlRegistrationType . $htmlPassType . $htmlDancerKind . $htmlLengthType . $htmlCompetitionParticipation . $htmlLocation . $htmlMerchandise . $htmlFormPrice .  $htmlClientName . $htmlClientEmail . $htmlClientPhone . $htmlClientCountry . $htmlClientComments . $htmlRegistrationdate . $htmlConfirmPrivateInformation . $htmlConfirmCovidResulte . 
                   "</div> </div>";
-  
+
   $message_footer =  "<div style='max-width: 550px; margin: 0 auto; line-height: 1.5rem; font-size: 0.9rem;'>"  . 
-                "<p>Další podrobnosti příkladu najdete na platformě REGIS. Prosíme, aby ste se registrace následně věnovali v nejbližší době. </p><br><br><br>" .
-  
                 "<h2 style='padding: 0; margin: 0; Margin: 0;text-align: left; line-height: 1.3; color: inherit; word-wrap: normal; font-family: Helvetica, Arial, sans-serif; margin-bottom: 10px; Margin-bottom: 10px; border-bottom: 3px solid #ffc600; padding-bottom: 10px;'>" .
-                "POKYNY PRO ADMINISTRACI</h2> Podanou žádost najdete v " . 
-                " <a href='#'> Sekce pro administrátora </a> ve správě registrací <br><br>" .
-                
-                "[ REGIS - Šimon Buryan 30. 10. 2021 ]" . "</p></div>";
-  
+                "Further infromation</h2>" . 
+                " <p>For further information please see the official <a href='#'> Facebook</a> event description. </p><br><br><br> <br><br>" .
+                "[ Platforma REGIS - Šimon Buryan 30. 10. 2021 ]" . "</p></div>";
+
   $message = $message_head . $message_body . $message_footer;
                 
   $replyto	=		"From: " . $clientEmail . " \n\r" . "Reply-To:" . $clientEmail . " \n\r";
-  
+
   $headers = "Content-Type: text/html; charset=UTF-8";
-  
+
   echo "<div class='container'> <div class='row'> <div class='col-lg-12 col-md-12 col-sm-12 padding-small'>";
   if (mail($to, $subject, $message, $headers)) {
-    echo "<h4 style='color:green;'>For buyer - The confirmation email was sent.</h4> <p style='color:green;'>Please check your email inbox, confirmation email could be in your spam box.</p>";
+    echo "<h4 style='color:green;'> The confirmation email with payment instructions was sent.</h4> <p style='color:green;'>Please check your email inbox, confirmation email could be in your spam box.</p>";
   } else {
-    echo "<h4 style='color:coral;'>For buyer - An error occured during sending confirming email.</h4> <p style='color:coral;'>Please check the event organizer for further information.</p>";
+    echo "<h4 style='color:coral;'>For buyer - An error occured during sending confirming email.</h4> <p style='color:coral;'>Please check the event organizer for further infromation.</p>";
   }
   echo "</div>";
 
-
+} else {
+  echo "<h4 style='color:coral;'>For buyer - An error occured during handling form data.</h4> <p style='color:coral;'>Please check the event organizer for further information.</p>";
+}
 ?>
