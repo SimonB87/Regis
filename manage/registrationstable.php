9443817e3329c0062d85a61d79df672fd103b29f
SimonB87 <buryans@gmail.com>
2021-10-30 19:42:14 +0200

    order table - set up

---
+++ manage/registrationstable.php
@@ -0,0 +1,29 @@
+<div class="bg-white padding-regular-topBottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+
+      <table id="mySearchTable" class="table tableDesign2019 search_project_table">
+        <thead>
+          <tr>
+            <th> ID </th>
+            <th> Event name </th>
+            <th> Client name </th>
+            <th> Client email </th>
+            <th> Client email </th>
+            <th data-breakpoints="xs sm md">Savings</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+
+
+        </tbody>
+      <table>
+
+
+      </div>
+    </div>
+  </div>
+</div>
