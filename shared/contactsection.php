23fbf3b35103604cff3f533f6e5cbdaf4a11e6f6
SimonB87 <buryans@gmail.com>
2021-10-31 22:40:16 +0100

    edite responsive contact section

--- shared/contactsection.php
+++ shared/contactsection.php
@@ -1,14 +1,14 @@
-<div class="bg-info">
+<div class="bg-info text-white">
     <div class="container text-center padding-small">
       <div class="row">
         <div class="col-12 padding-small">
           <h2>Event organizer</h2>
           <p>Mauritius & Elvira Bachata Prague <p>
         </div>
-        <div class="col-6 padding-small">
+        <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
           <p><strong>Email : </strong> mauritius.elvira[at]gmail.com  <p>
         </div>
-        <div class="col-6 padding-small">
+        <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
           <p><strong>Phone, WhatsApp, Telegram : </strong> + 420 777 216 875  <p>
         </div>
 
