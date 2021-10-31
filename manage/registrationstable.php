ed7009a1175c1e9610e0c350b166b91859fcc218
SimonB87 <buryans@gmail.com>
2021-10-31 22:04:37 +0100

    registration table

--- manage/registrationstable.php
+++ manage/registrationstable.php
@@ -1,26 +1,101 @@
+
 <div class="bg-white padding-regular-topBottom">
   <div class="container">
     <div class="row">
+      <div class="col-12">  
+        <h1 class="text-center padding-small">Saved registrations</h1>
+      </div>
       <div class="col-12">
 
-      <table id="mySearchTable" class="table tableDesign2019 search_project_table">
-        <thead>
+      <table id="orders" class="table tableDesign2019 search_project_table">
+        <thead class="bg-info">
           <tr>
-            <th> ID </th>
-            <th> Event name </th>
-            <th> Client name </th>
-            <th> Client email </th>
-            <th> Client email </th>
-            <th data-breakpoints="xs sm md">Savings</th>
+            <th class="footable-sortable"> ID </th>
+            <th class="footable-sortable" data-breakpoints="xs sm"> Event name </th>
+            <th class="footable-sortable"> Registration date </th>
+            <th class="footable-sortable"> Client name </th>
+            <th class="footable-sortable" data-breakpoints="xs sm md"> Pass type </th>
+            <th class="footable-sortable"> Price </th>
+            <th class="footable-sortable" data-breakpoints="xs sm md"> Client email </th>
+            <th class="footable-sortable" data-breakpoints="xs sm md"> Dancer kind </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Registration type </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md"> Client phone </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Length type </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Client country </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Client comments </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Client GDRP confirm </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Client COVID policy confirm </th> 
+            <th class="footable-sortable" data-breakpoints="xs sm md lg"> Competition participation </th>
+            <th class="footable-sortable" data-breakpoints="xs sm md"> Merchandise </th>
+            <th class="footable-sortable" data-breakpoints="xs sm"> Payment status </th>
           </tr>
         </thead>
 
         <tbody>
 
+        <?php
+
+          if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+          }
+
+          /* debug
+          if (!mysqli_query($connector, "SET a=1")) {
+            printf("Error message: %s\n", mysqli_error($link));
+          }*/
+
+
+          if (!mysqli_set_charset($connector, "utf8")) {
+            printf("Error loading character set utf8: %s\n", mysqli_error($con));
+            exit();
+            } else {
+                    //printf("Current character set: %s\n", mysqli_character_set_name($con));//used only for testing
+            }
+
+            $sql = "SELECT * FROM registrations ORDER BY registrationdate DESC";
+
+            $results = $connector-> query($sql);
+            //Error case
+            if (!$sql ) {
+              echo "Failed! <br> Error sql: " . mysql_error();
+            }
+            
+            
+            if (mysqli_query($connector, $sql)) {
+              //debug echo json_encode(array("statusCode"=>200));
+            } 
+            else {
+              echo json_encode(array("sql - statusCode"=>418));
+            }
+
+            $paymentStatus = "<form><select class='form-select' aria-label='Default select example'>
+                                <option selected selected>TODO - finish</option>
+                                <option value='1 - unpaid'>Unpaid</option>
+                                <option value='2 - paid'>Paid</option>
+                                <option value='3 - reminder sent'>Reminder sent</option>
+                              </select></form>";
+
+            //function to fatch the data
+            if ($results-> num_rows > 0 ) {
+              while ($row = $results-> fetch_assoc()) {
+
+                  echo "<tr><td>" . $row["id"] . "</td><td>" . $row["eventName"] . "</td><td>" . $row["registrationdate"]  . "</td><td>" . $row["clientName"] ."</td><td>" . $row["passType"] . "</td> <td>" . $row["formPrice"] . "</td> <td>" . $row["clientEmail"]  . "</td> <td>" . $row["dancerKind"] . "</td><td>" . 
+                        $row["registrationType"] . "</td><td>" . $row["clientPhone"] . "</td><td>" . $row["lengthType"] . "</td><td>" . $row["clientCountry"] . "</td><td>" . $row["clientComments"] . "</td><td>" . $row["confirmPrivateInformation"] . "</td><td>" .
+                        $row["confirmCovid"] . "</td><td>" . $row["competitionParticipation"] . "</td><td>" . $row["merchandise"] . "</td><td>" . $paymentStatus . "</td>" . "</tr>";
+              }
+              echo "";
+          }
+          else {
+              echo "There are 0 results in DB table";
+          }
+
+          mysqli_close($connector);
+        ?>
 
 
         </tbody>
-      <table>
+      </table>
 
 
       </div>
--- registrations.php
+++ registrations.php
@@ -1,3 +1,9 @@
+<?php
+
+require 'config/config.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,6 +11,8 @@
     <?php
     include("shared/head.php");
     ?>
+    <link rel="stylesheet" type="text/css" href="shared/libs/footablebootstrap/css/footable.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="shared/assets/css/footbaleedits.css">
 
     <title> Event registration | 🕺💃🏻 Mauritius & Elvira Bachata Prague </title>
 
@@ -27,13 +35,19 @@
     include("shared/footer.php");
     ?>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script src="shared/libs/bootstrap/js/bootstrap.js"></script>
+
 
   <!--
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-    <script src="shared/assets/js/formvalidation.js"></script>
-    <script src="shared/assets/js/recalculateprice.js"></script>
-    <script src="shared/assets/js/setdate.js"></script>
+    <script src="shared/libs/footablebootstrap/js/footable.js" defer></script>
+    <script defer>
+    jQuery(function($){
+        $('.table').footable();
+      });
+    /* Sollution guide 15.10.2020: http://fooplugins.github.io/FooTable/docs/examples/basic/single-header.html */
+    </script>
 
   </body>
 </html>
