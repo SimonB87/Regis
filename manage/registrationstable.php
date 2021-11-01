c157ccf25b0183abea483f0f39e1014e51565af3
SimonB87 <buryans@gmail.com>
2021-11-01 19:38:25 +0100

    registration table edit order of results

--- manage/registrationstable.php
+++ manage/registrationstable.php
@@ -54,7 +54,7 @@
                     //printf("Current character set: %s\n", mysqli_character_set_name($con));//used only for testing
             }
 
-            $sql = "SELECT * FROM registrations ORDER BY orderId DESC";
+            $sql = "SELECT * FROM registrations ORDER BY id DESC";
 
             $results = $connector-> query($sql);
             //Error case
