36ecc50104fd30aa00b9e1292f37aee221e1e49d
SimonB87 <buryans@gmail.com>
2021-11-07 15:41:54 +0100

    Style file - counted versions

--- shared/head.php
+++ shared/head.php
@@ -5,5 +5,5 @@
   <link href="shared/libs/bootstrap/css/bootstrap.css" rel="stylesheet" >
 <!--
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-  <link href="shared/assets/css/style.css" rel="stylesheet" >
+  <link href="shared/assets/css/style3.css" rel="stylesheet" >
   <link rel="shortcut icon" href="/shared/me.ico" type="image/x-icon">
