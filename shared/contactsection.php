ea5c0634fe465bf7c7599fbf576f5f2f3cba42bd
SimonB87 <buryans@gmail.com>
2021-11-10 12:49:12 +0100

    esited contacts strip

--- shared/contactsection.php
+++ shared/contactsection.php
@@ -3,13 +3,39 @@
       <div class="row">
         <div class="col-12 padding-small">
           <h2>Event organizer</h2>
-          <p><a class="text-white" href="https://www.mauritiuselvira.com/">Mauritius & Elvira Bachata Prague </a><p>
+          <div><p>Mauritius & Elvira Bachata Prague<p></div>
+
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
+              <p><a href="https://www.mauritiuselvira.com/" target="_blank" class="text-white">Mauritius & Elvira Web </a><p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
+            <p><a href="https://www.facebook.com/bachata.mauritius.elvira" target="_blank" class="text-white">Mauritius & Elvira Facebook </a><p>
+            </div>
+          </div>
+
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
-          <p><strong>Email : </strong> mauritius.elvira[at]gmail.com  <p>
+
+          <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12 padding-small">
+              <strong>Email : </strong>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12 padding-small">
+              <p> mauritius.elvira[at]gmail.com  <p>
+            </div>
+          </div>
+
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 padding-small">
-          <p><strong>Phone, WhatsApp, Telegram : </strong> + 420 777 216 875  <p>
+        <div class="row">
+            <div class="col-xl-6 col-lg-8 col-md-8 col-sm-12 padding-small">
+              <strong>Phone, WhatsApp, Telegram  : </strong>
+            </div>
+            <div class="col-xl-6 col-lg-4 col-md-4 col-sm-12 padding-small">
+              <p> + 420 777 216 875 <p>
+            </div>
+          </div>
         </div>
 
       </div>
