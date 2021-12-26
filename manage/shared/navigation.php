6a94d688d5dc564e3fd9bb6ff8b3fae77887f384
SimonB87 <buryans@gmail.com>
2021-12-26 16:53:31 +0100

    admin navigation

---
+++ manage/shared/navigation.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark underline-white">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="https://www.mauritiuselvira.com/">
+        <img src="https://uploads-ssl.webflow.com/5e18aa9988b6fcc053b834a9/5e79e6b7afefe444bb6e516a_page-01__1_-removebg-preview.png" sizes="226px" srcset="https://uploads-ssl.webflow.com/5e18aa9988b6fcc053b834a9/5e79e6b7afefe444bb6e516a_page-01__1_-removebg-preview-p-500.png 500w, https://uploads-ssl.webflow.com/5e18aa9988b6fcc053b834a9/5e79e6b7afefe444bb6e516a_page-01__1_-removebg-preview-p-800.png 800w, https://uploads-ssl.webflow.com/5e18aa9988b6fcc053b834a9/5e79e6b7afefe444bb6e516a_page-01__1_-removebg-preview.png 866w" alt="mauritiuselvira.com logo" class="logo" width="226">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse full-width-lg" id="navbarColor01">
+        <ul class="navbar-nav justify-content-center full-width">
+          <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Admin home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="../index.php">Event home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Event options
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Event main set up</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Event description</a></li>
+              <li><a class="dropdown-item" href="#">Event register form </a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registrations.php">Registrations</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.mauritiuselvira.com/contact">Contact</a>
+          </li>
+
+<?php 
+        if (isset($_SESSION['username'])) {
+          echo "<li class='nav-item'><a href='handlers\logout.php' class='nav-link'>
+                  Logout
+                </a></li>";
+        } else {
+          echo "<li class='nav-item'>Not loggedin</li>";
+        }
+?>
+
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
