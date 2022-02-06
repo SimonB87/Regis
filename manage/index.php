84f9b111161aa09a916fa6ba16a4617d1efdff80
SimonB87 <buryans@gmail.com>
2022-02-06 16:10:42 +0100

    admin home - section on event registration statistics - centered

--- manage/index.php
+++ manage/index.php
@@ -50,11 +50,16 @@ if (!mysqli_set_charset($connector, "utf8")) {
     <!-- TODO - statistics about current event registrations-->
     <div class="container px-4 py-5 my-5">
       <div class="row">
-        <h2 class="text-center display-7 fw-bold">Current event summary</h2>
-        <p>Number of registrations: <span class="fw-bold">20</span><p>
-        <p>Paid registrations: <span class="fw-bold">15</span></p>
-        <p>Registrations waiting for payment: <span class="fw-bold">3</span></p>
-        <p>Delayed registration payments: <span class="fw-bold">3</span></p>
+        <div class="col-12">
+          <h2 class="text-center display-7 fw-bold">Current event summary</h2>
+        </div>
+        <div class="col-6 center-margin">
+          <p>Event name: <span class="fw-bold">Tester & Testie Easter Prague Bachata Weekend</span></p>
+          <p>Number of registrations: <span class="fw-bold">20</span><p>
+          <p>Paid registrations: <span class="fw-bold">15</span></p>
+          <p>Registrations waiting for payment: <span class="fw-bold">3</span></p>
+          <p>Delayed registration payments: <span class="fw-bold">3</span></p>
+        </div>
       </div>
     </div>
     </main>
