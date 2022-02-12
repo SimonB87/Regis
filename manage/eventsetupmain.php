4e571b4e9d62e54c52bbfb4fff82416f956bf377
SimonB87 <buryans@gmail.com>
2022-02-12 19:18:14 +0100

    table for past events reading from db

--- manage/eventsetupmain.php
+++ manage/eventsetupmain.php
@@ -103,24 +103,61 @@ if (!mysqli_set_charset($connector, "utf8")) {
           </thead>
 
           <tbody>
-            <!-- TODO : show data in tabel from database -->
-            <tr>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-              <td> # </td>
-            </tr>
+
+          <?php
+
+          if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+          }
+
+          /* debug
+          if (!mysqli_query($connector, "SET a=1")) {
+            printf("Error message: %s\n", mysqli_error($link));
+          }*/
+
+
+          if (!mysqli_set_charset($connector, "utf8")) {
+            printf("Error loading character set utf8: %s\n", mysqli_error($con));
+            exit();
+            } else {
+                    //printf("Current character set: %s\n", mysqli_character_set_name($con));//used only for testing
+            }
+
+            $sql = "SELECT * FROM events ORDER BY id DESC";
+
+            $results = $connector-> query($sql);
+            //Error case
+            if (!$sql ) {
+              echo "Failed! <br> Error sql: " . mysql_error();
+            }
+            
+            
+            if (mysqli_query($connector, $sql)) {
+              //debug echo json_encode(array("statusCode"=>200));
+            } 
+            else {
+              echo json_encode(array("sql - statusCode"=>418));
+            }
+
+
+            //function to fatch the data
+            if ($results-> num_rows > 0 ) {
+              while ($row = $results-> fetch_assoc()) {
+
+                  echo "<tr><td>" . $row["id"] . "</td><td>" . $row["eventStatus"] . "</td><td>" . $row["eventName"]  . "</td><td>" . $row["eventStartDate"] ."</td><td>" . $row["eventEndDate"] . "</td> <td>" . $row["enableCoupleTicket"] . "</td>" . 
+                      "<td> # </td> <td> # </td> <td> # </td> <td> # </td> <td> # </td> <td> # </td> <td> # </td> <td> # </td> <td> # </td>" .
+                      "</tr>";
+              }
+              echo "";
+          }
+          else {
+              echo "There are 0 results in DB table";
+          }
+
+          mysqli_close($connector);
+          ?>
+
           </tbody>
         </table>
 
