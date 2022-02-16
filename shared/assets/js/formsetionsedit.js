e7deaa2940a5a51c1fd22f29b560d42b1e87f435
SimonB87 <buryans@gmail.com>
2022-02-16 02:17:37 +0100

    JS form sections hide/shoe edited

--- shared/assets/js/formsetionsedit.js
+++ shared/assets/js/formsetionsedit.js
@@ -8,34 +8,53 @@ function toggleVisibility(icon, section) {
   });
 }*/
 
+/** TODO - show / hide refactor -  **/
 $(document).ready(function(){
 
-  $(".section-icon-1:nth-child(1), .section-icon-2:nth-child(2), .section-icon-3:nth-child(2), .section-icon-4:nth-child(2), .section-icon-5:nth-child(2)").hide();
-  $(".section-form-2, .section-form-3, .section-form-4, .section-form-5").hide();
-//"slow"
+  var segments = $(".show-segment--icon");
+  var segmemtsAmount = Math.ceil(segments.length * 0.5);
+
+  $(".section-icon-1:nth-child(1)").hide();
+
+  for (var i = 0; i < segmemtsAmount; i++) {
+    var c = i + 1;
+
+    if (c > 1) {
+      $(".section-icon-" + c +":nth-child(2)").hide();
+      $(".section-form-" + c ).hide();
+    }
+
+  }
+
+  // hode/show
   $(".section-icon-1").click(function(){
     $(".section-form-1").slideToggle();
     $(".section-icon-1").toggle();
   });
-//"slow"
+
   $(".section-icon-2").click(function(){
     $(".section-form-2").slideToggle();
     $(".section-icon-2").toggle();
   });
-//"slow"
+
   $(".section-icon-3").click(function(){
     $(".section-form-3").slideToggle();
     $(".section-icon-3").toggle();
   });
-//"slow"
+
   $(".section-icon-4").click(function(){
     $(".section-form-4").slideToggle();
     $(".section-icon-4").toggle();
   });
-//"slow"
+
   $(".section-icon-5").click(function(){
     $(".section-form-5").slideToggle();
     $(".section-icon-5").toggle();
+  });
+
+  $(".section-icon-6").click(function(){
+    $(".section-form-6").slideToggle();
+    $(".section-icon-6").toggle();
   });
 
 });
