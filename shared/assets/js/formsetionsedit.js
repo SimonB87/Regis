467affa43ecbc225b0a03bc3b1d8e9ded41d6e8d
SimonB87 <buryans@gmail.com>
2022-02-16 11:29:41 +0100

    JS - Show / Hide sections

--- shared/assets/js/formsetionsedit.js
+++ shared/assets/js/formsetionsedit.js
@@ -18,15 +18,20 @@ $(document).ready(function(){
     // hide/show segments
     $(".show-segment--icon").click(function(){
       var _this = this;
-      var classes = _this.classList;
-      classes.remove("show-segment--icon");
-      var classItem = classes[0];
-      var targetIndex = classItem[ classItem.length - 1 ];
+      var classesLength = _this.classList.length;
+      var classesItems = [];
+
+      for (var n = 0; n < classesLength; n++ ) {
+        classesItems.push(_this.classList[n]);
+      }
+
+      classesItems.splice( (classesItems.indexOf("show-segment--icon")), 1 );
+
+      var targetIndex = classesItems[0][((classesItems[0].length) - 1)];
 
       $(".section-form-" + targetIndex).slideToggle();
       $(".section-icon-" + targetIndex).toggle();
 
-      _this.classList.add("show-segment--icon");
     });
 
 });
