fdfeb30cc5449bf4e47f3cf35ddf90eae204fb63
SimonB87 <buryans@gmail.com>
2022-02-23 21:24:26 +0100

    Aside - edited

--- manage/shared/aside.php
+++ manage/shared/aside.php
@@ -1,26 +1,26 @@
 <aside class="aside-container">
 
-  <div class="full-width padding-small" id="hide-aside"> 
-    <i class="far fas fa-backward aside-icon padding-small " aria-hidden="true">
-      <span class="aside-caption hidden padding-small-left"> Hide/Show menu </span>
+  <div id="hide-aside" class="full-width padding-small" > 
+    <i class="far fas fa-backward aside-icon padding-small rotating-icon" aria-hidden="true">
+      <span class="aside-caption hidden padding-small-leftRight rotating-caption"> Hide/Show menu </span>
     </i> 
   </div>
 
-  <div class="full-width toggleble padding-small"> 
+  <div id="hide-revenuetable" class="full-width toggleble padding-small"> 
     <i class="fas fa-hand-holding-usd aside-icon padding-small " aria-hidden="true">
-      <span class="aside-caption hidden padding-small-left"> Display Revenue Estimates </span>
+      <span class="aside-caption hidden padding-small-leftRight"> Display Revenue Estimates </span>
     </i> 
   </div>
 
-  <div class="full-width toggleble padding-small"> 
+<!--   <div class="full-width toggleble padding-small"> 
     <i class="far fa-comment aside-icon padding-small " aria-hidden="true">
-      <span class="aside-caption hidden padding-small-left"> Display Help </span>
+      <span class="aside-caption hidden padding-small-leftRight"> Display Help </span>
     </i> 
-  </div>
+  </div> -->
 
   <div class="full-width toggleble padding-small"> 
     <i class="fas fa-file-alt aside-icon padding-small " aria-hidden="true">
-      <span class="aside-caption hidden padding-small-left"> Download PDF User Manual </span>
+      <span class="aside-caption hidden padding-small-leftRight"> Download PDF User Manual </span>
     </i> 
   </div>
 
