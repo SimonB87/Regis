d9dc88b4ef4729f102ded13ea10e224d901196ae
SimonB87 <buryans@gmail.com>
2022-02-24 14:50:23 +0100

    tabel event summary data - edited JS sum - nums with spaces

--- shared/assets/js/scriptsformevent.js
+++ shared/assets/js/scriptsformevent.js
@@ -75,20 +75,20 @@ function recountRevenueData() {
   const specialType2TicketPriceSingle = getInputValue("specialType2TicketPriceSingle");
   const specialType2TicketPriceCouple = getInputValue("specialType2TicketPriceCouple");
 
-  document.querySelector("#earlybirds-people").innerText = Number ( ticketsAmountEarlyBirdsRegistrationsSingle) + (2 * Number(earlyBirdsTicketAmountCouple));
-  document.querySelector("#regular-people").innerText = Number ( regularTicketAmountSingle ) + (2 * Number (regularTicketAmountCouple));
-  document.querySelector("#party-people").innerText = Number ( partyTicketAmountSingle ) + (2 * Number (partyTicketAmountCouple));
-  document.querySelector("#special1-people").innerText = Number ( specialType1TicketAmountSingle ) + (2 * Number (specialType1TicketAmountCouple));
-  document.querySelector("#special2-people").innerText = Number ( specialType2TicketAmountSingle ) + (2 * Number (specialType2TicketAmountCouple));
+  document.querySelector("#earlybirds-people").innerText = numberWithSpaces(Number ( ticketsAmountEarlyBirdsRegistrationsSingle) + (2 * Number(earlyBirdsTicketAmountCouple)));
+  document.querySelector("#regular-people").innerText = numberWithSpaces(Number ( regularTicketAmountSingle ) + (2 * Number (regularTicketAmountCouple)));
+  document.querySelector("#party-people").innerText = numberWithSpaces(Number ( partyTicketAmountSingle ) + (2 * Number (partyTicketAmountCouple)));
+  document.querySelector("#special1-people").innerText = numberWithSpaces(Number ( specialType1TicketAmountSingle ) + (2 * Number (specialType1TicketAmountCouple)));
+  document.querySelector("#special2-people").innerText = numberWithSpaces(Number ( specialType2TicketAmountSingle ) + (2 * Number (specialType2TicketAmountCouple)));
 
-  document.querySelector("#earlybirds-money").innerText = (Number( Number(ticketsAmountEarlyBirdsRegistrationsSingle) * Number(earlyBirdsTicketPriceSingle) ) + Number( Number(earlyBirdsTicketPriceCouple) * Number(earlyBirdsTicketAmountCouple) ));
-  document.querySelector("#regular-money").innerText = (Number( Number(regularTicketAmountSingle) * Number(regularTicketPriceSingle) ) + Number( Number(regularTicketAmountCouple) * Number(regularTicketPriceCouple) ));
-  document.querySelector("#party-money").innerText = (Number( Number(partyTicketAmountSingle) * Number(partyTicketPriceSingle) ) + Number( Number(partyTicketPriceCouple) * Number(partyTicketAmountCouple) ));
-  document.querySelector("#special1-money").innerText = (Number( Number(specialType1TicketAmountSingle) * Number(specialType1TicketPriceSingle) ) + Number( Number(specialType1TicketAmountCouple) * Number(specialType1TicketPriceCouple) ));
-  document.querySelector("#special2-money").innerText = (Number( Number(specialType2TicketAmountSingle) * Number(specialType2TicketPriceSingle) ) + Number( Number(specialType2TicketAmountCouple) * Number(specialType2TicketPriceCouple) ));
+  document.querySelector("#earlybirds-money").innerText = numberWithSpaces(Number( Number(ticketsAmountEarlyBirdsRegistrationsSingle) * Number(earlyBirdsTicketPriceSingle) ) + Number( Number(earlyBirdsTicketPriceCouple) * Number(earlyBirdsTicketAmountCouple) ));
+  document.querySelector("#regular-money").innerText = numberWithSpaces(Number( Number(regularTicketAmountSingle) * Number(regularTicketPriceSingle) ) + Number( Number(regularTicketAmountCouple) * Number(regularTicketPriceCouple) ));
+  document.querySelector("#party-money").innerText = numberWithSpaces(Number( Number(partyTicketAmountSingle) * Number(partyTicketPriceSingle) ) + Number( Number(partyTicketPriceCouple) * Number(partyTicketAmountCouple) ));
+  document.querySelector("#special1-money").innerText = numberWithSpaces(Number( Number(specialType1TicketAmountSingle) * Number(specialType1TicketPriceSingle) ) + Number( Number(specialType1TicketAmountCouple) * Number(specialType1TicketPriceCouple) ));
+  document.querySelector("#special2-money").innerText = numberWithSpaces(Number( Number(specialType2TicketAmountSingle) * Number(specialType2TicketPriceSingle) ) + Number( Number(specialType2TicketAmountCouple) * Number(specialType2TicketPriceCouple) ));
 
-  const totalPeopleCount = Number (getInputValue("earlybirds-people", false)) + Number (getInputValue("regular-people", false)) + Number (getInputValue("party-people", false)) + Number (getInputValue("special1-people", false)) + Number (getInputValue("special2-people", false));
-  const summaryMoney = Number (getInputValue("earlybirds-money", false)) + Number (getInputValue("regular-money", false)) + Number (getInputValue("party-money", false)) + Number (getInputValue("special1-money", false)) + Number (getInputValue("special2-money", false));
+  const totalPeopleCount = numberWithSpaces(Number (getInputValue("earlybirds-people", false)) + Number (getInputValue("regular-people", false)) + Number (getInputValue("party-people", false)) + Number (getInputValue("special1-people", false)) + Number (getInputValue("special2-people", false)));
+  const summaryMoney = numberWithSpaces(Number (getInputValue("earlybirds-money", false)) + Number (getInputValue("regular-money", false)) + Number (getInputValue("party-money", false)) + Number (getInputValue("special1-money", false)) + Number (getInputValue("special2-money", false)));
 
   document.querySelector("#summary-people").innerText = totalPeopleCount;
   document.querySelector("#summary-money").innerText = summaryMoney;
@@ -99,9 +99,16 @@ function recountRevenueData() {
         result = document.getElementById(name).value;
       } else {
         result = document.getElementById(name).innerText;
+        result = Number(result.replace(' ',''));
       }
       return result;
     }
+
+    function numberWithSpaces(num) {
+      const parts = num.toString().split(".");
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+      return parts.join(".");
+  }
 
 }
 
