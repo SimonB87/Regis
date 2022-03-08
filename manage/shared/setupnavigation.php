97fe14f74240f941d0e5b8a8d5ca1769d9ec9473
SimonB87 <buryans@gmail.com>
2022-03-08 01:09:42 +0100

    edited navigation order

--- manage/shared/setupnavigation.php
+++ manage/shared/setupnavigation.php
@@ -13,8 +13,8 @@
   <div class="col-12 text-center">
     <a id="nav_eventsetupmain" href=<?php if($isModeEventCreate) {echo "createneweventsetupmain.php"; } else {echo "eventsetupmain.php"; } ?> class="weblink padding-small "> Event main set up </a>
     <a id="nav_eventdescription" href=<?php if($isModeEventCreate) {echo "createneweventeventdescription.php"; } else {echo "eventdescription.php"; } ?> class="weblink padding-small"> Event description </a>
-    <a id="nav_eventform" href=<?php if($isModeEventCreate) {echo "createneweventeventformpreview.php"; } else {echo "eventformpreview.php"; } ?> class="weblink padding-small"> Event register form </a>
     <a id="nav_merchandise" href="merchendasesetup.php" class="weblink padding-small"> Merchendase set-up </a>
+    <a id="nav_eventform" href=<?php if($isModeEventCreate) {echo "createneweventeventformpreview.php"; } else {echo "eventformpreview.php"; } ?> class="weblink padding-small"> Event register form </a>
   </div>
 
 </div>
