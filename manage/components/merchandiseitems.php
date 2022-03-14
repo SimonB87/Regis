5c9f8fd1d7effab9be47f90319fda2c3ad3d443f
SimonB87 <buryans@gmail.com>
2022-03-14 23:33:03 +0100

    merchandise items - added item 3 and 4, removed delete button

--- manage/components/merchandiseitems.php
+++ manage/components/merchandiseitems.php
@@ -52,15 +52,14 @@
             </div>
           </div>
 
-
           <div class="col-12 margin-small-topBottom">
             <input class="btn btn-primary full-height full-width" type="submit" name="safe_form_data1" value="Update Merch Item 1">
           </div>
-
+          <!--
           <div class="col-12 margin-small-topBottom">
-            <button type="button full-width padding-small-topBottom full-width" class="btn btn-danger">Delete Item</button>
+            <button class="btn button full-width padding-small-topBottom full-width btn btn-danger">Delete Item</button>
           </div>
-
+          -->
         </div>
       </form>
 
@@ -127,11 +126,151 @@
           <div class="col-12 margin-small-topBottom">
             <input class="btn btn-primary full-height full-width padding-small-topBottom" type="submit" name="safe_form_data2" value="Update Merch Item 2">
           </div>
-
+          <!--
           <div class="col-12 margin-small-topBottom">
-            <button type="button full-width padding-small-topBottom full-width" class="btn btn-danger" >Delete Item</button>
+            <button class="btn button full-width padding-small-topBottom full-width btn btn-danger">Delete Item</button>
+          </div>
+          -->
+        </div>
+      </form>
+
+    </div>
+  </div>
+
+</div>
+
+<div class="col-sm-12 col-md-6 col-xl-3 padding-regular-topBottom ">
+
+  <div class="card full-width shadow-light">
+    <div class="card_top_shade"></div>
+    <div class="card-body">
+      <h5 class="card-title"> Merch Item 3 </h5>
+      <form id="set_merch1" class="needs-validation bts-form full-width" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
+        <div class="row full-width">
+
+          <div class="col-12">
+            <div class="form-floating padding-small-topBottom">
+              <input type="text" class="form-control" id="merch_name_item1" name="merch_name_item1" maxlength="250" <?php /*if(isset($eventDataPreviewDate)) {echo "value='" . $eventDataPreviewDate . "'"; } */ ?> >
+              <label for="merch_name_item1"> Merch Item 3 Name</label>
+              <div class="valid-feedback">
+                  Looks good!
+                </div>
+                <div class="invalid-feedback">
+                  Please fill in valid value.
+                </div>
+            </div>
           </div>
 
+          <div class="col-12">
+            <div class="form-floating padding-small-topBottom">
+              <input type="text" class="form-control" id="merch_price_item1" name="merch_price_item1" maxlength="250" <?php /*if(isset($eventDataPreviewDate)) {echo "value='" . $eventDataPreviewDate . "'"; } */ ?> >
+              <label for="merch_price_item1"> Merch Item 3 Price</label>
+              <div class="valid-feedback">
+                  Looks good!
+                </div>
+                <div class="invalid-feedback">
+                  Please fill in valid value.
+                </div>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="mb-3 full-width full-height div-select">
+              <select id="merch_available_item1" class="form-select" aria-label="Pass type" required name="merch_available_item1" >
+                <option value="" selected>Select item status ...</option>
+                <option value="0 - Item not offered" aria-valuenow="0" <?php if(isset($isEventEdited)) { if( $eventDataEventStatus == "0 - Registrations closed") { echo "selected"; } } ?> > Item not offered </option>
+                <option value="1 - Item available" aria-valuenow="0" <?php if(isset($isEventEdited)) { if( $eventDataEventStatus == "1 - Registrations open") { echo "selected"; } } ?> > Item available </option>
+              </select>
+              <div class="form-select-customLabel">
+                Item Available
+              </div>
+              <div class="valid-feedback">
+                Looks good!
+              </div>
+              <div class="invalid-feedback">
+                Please select some valid option.
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 margin-small-topBottom">
+            <input class="btn btn-primary full-height full-width" type="submit" name="safe_form_data3" value="Update Merch Item 3">
+          </div>
+          <!--
+          <div class="col-12 margin-small-topBottom">
+            <button class="btn button full-width padding-small-topBottom full-width btn btn-danger">Delete Item</button>
+          </div>
+          -->
+        </div>
+      </form>
+
+    </div>
+  </div>
+
+</div>
+
+<div class="col-sm-12 col-md-6 col-xl-3 padding-regular-topBottom ">
+
+  <div class="card full-width shadow-light">
+    <div class="card_top_shade"></div>
+    <div class="card-body">
+      <h5 class="card-title"> Merch Item 4 </h5>
+      <form id="set_merch1" class="needs-validation bts-form full-width" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
+        <div class="row full-width">
+
+          <div class="col-12">
+            <div class="form-floating padding-small-topBottom">
+              <input type="text" class="form-control" id="merch_name_item1" name="merch_name_item1" maxlength="250" <?php /*if(isset($eventDataPreviewDate)) {echo "value='" . $eventDataPreviewDate . "'"; } */ ?> >
+              <label for="merch_name_item1"> Merch Item 4 Name</label>
+              <div class="valid-feedback">
+                  Looks good!
+                </div>
+                <div class="invalid-feedback">
+                  Please fill in valid value.
+                </div>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="form-floating padding-small-topBottom">
+              <input type="text" class="form-control" id="merch_price_item1" name="merch_price_item1" maxlength="250" <?php /*if(isset($eventDataPreviewDate)) {echo "value='" . $eventDataPreviewDate . "'"; } */ ?> >
+              <label for="merch_price_item1"> Merch Item 4 Price</label>
+              <div class="valid-feedback">
+                  Looks good!
+                </div>
+                <div class="invalid-feedback">
+                  Please fill in valid value.
+                </div>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="mb-3 full-width full-height div-select">
+              <select id="merch_available_item1" class="form-select" aria-label="Pass type" required name="merch_available_item1" >
+                <option value="" selected>Select item status ...</option>
+                <option value="0 - Item not offered" aria-valuenow="0" <?php if(isset($isEventEdited)) { if( $eventDataEventStatus == "0 - Registrations closed") { echo "selected"; } } ?> > Item not offered </option>
+                <option value="1 - Item available" aria-valuenow="0" <?php if(isset($isEventEdited)) { if( $eventDataEventStatus == "1 - Registrations open") { echo "selected"; } } ?> > Item available </option>
+              </select>
+              <div class="form-select-customLabel">
+                Item Available
+              </div>
+              <div class="valid-feedback">
+                Looks good!
+              </div>
+              <div class="invalid-feedback">
+                Please select some valid option.
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 margin-small-topBottom">
+            <input class="btn btn-primary full-height full-width" type="submit" name="safe_form_data4" value="Update Merch Item 4">
+          </div>
+          <!--
+          <div class="col-12 margin-small-topBottom">
+            <button class="btn button full-width padding-small-topBottom full-width btn btn-danger">Delete Item</button>
+          </div>
+          -->
         </div>
       </form>
 
