cb841d7b079688a485b9d196d75084fe9b493e26
SimonB87 <buryans@gmail.com>
2022-05-19 19:46:44 +0200

    set current page event poster

--- shared/assets/js/seteventsetupcurrentpage.js
+++ shared/assets/js/seteventsetupcurrentpage.js
@@ -2,16 +2,18 @@ setCurrentNavigation();
 
 function setCurrentNavigation() {
   const currentPage = window.location.pathname;
-  
+
   const pathMain = "eventsetupmain.php";
   const pathDescription = "eventdescription.php";
   const pathEventForm = "eventformpreview.php";
   const pathEventMerch = "merchandisesetup.php";
+  const pathPoster = "eventposter.php";
 
   const isPageMain = currentPage.search(pathMain);
   const isPageDescription = currentPage.search(pathDescription);
   const isPageEventForm = currentPage.search(pathEventForm);
   const isPageMerch = currentPage.search(pathEventMerch);
+  const isPagePoster = currentPage.search(pathPoster);
 
   if (isPageMain > 0) {
     document.getElementById("nav_eventsetupmain").classList.add("fw-bold");
@@ -27,6 +29,10 @@ function setCurrentNavigation() {
 
   if (isPageMerch > 0) {
     document.getElementById("nav_merchandise").classList.add("fw-bold");
+  }
+
+  if (isPagePoster > 0) {
+    document.getElementById("nav_eventposter").classList.add("fw-bold");
   }
 
 }
