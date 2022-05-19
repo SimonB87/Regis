1d4b89ca0b604c6d93107166b922ea7118b750ad
SimonB87 <buryans@gmail.com>
2022-05-19 22:33:48 +0200

    edit format datepicker - remowed

--- shared/assets/js/editformatdatepicker.js
+++ shared/assets/js/editformatdatepicker.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
-  $('input[type=date]').datepicker({ dateFormat: 'dd/mm/yyyy' }); // format to
+$(document).ready(function () {
+  //$('input[type=date]').datepicker({ dateFormat: 'dd/mm/yyyy' }); // format to
   // $('input[type=date]').datepicker({ format: 'dd/mm/yyyy' }); // format to
 });
