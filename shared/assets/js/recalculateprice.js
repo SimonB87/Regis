fcb3b61f594626b933896f7fa142806a6158c812
SimonB87 <buryans@gmail.com>
2022-07-19 23:27:21 +0200

    preview form - edited prefill prices

--- shared/assets/js/recalculateprice.js
+++ shared/assets/js/recalculateprice.js
@@ -1,89 +1,43 @@
 'use strict'
 function recalculatePrice() {
-  const registrationType = document.getElementById("registrationType");
   const passType = document.getElementById("passType");
   const dancerKind = document.getElementById("dancerKind");
-  const lengthType = document.getElementById("lengthType");
-  const competitionParticipation = document.getElementById("competitionParticipation");
-  const merchandise = document.getElementById("merchandise");
-
+  const otherTicketOptions = document.getElementById("otherTicketOptions");
+  //const lengthType = document.getElementById("lengthType");
+  //const merchandise = document.getElementById("merchandise");
+  //const registrationType = document.getElementById("registrationType");
 
   //const allCustomerOptionsWereSelected = registrationType.checkValidity() && passType.checkValidity() && dancerKind.checkValidity() && lengthType.checkValidity() && competitionParticipation.checkValidity() && merchandise.checkValidity();
-  const allCustomerOptionsWereSelected = registrationType.checkValidity() && passType.checkValidity() && competitionParticipation.checkValidity() && merchandise.checkValidity();
-
+  const allCustomerOptionsWereSelected = passType.checkValidity() && dancerKind.checkValidity() && otherTicketOptions.checkValidity();
 
   if (allCustomerOptionsWereSelected) {
 
     const mainPrice = getMainPrice();
-    const priceCoefficient = getPriceCoefficient();
     const additionalService = getAdditionalService();
-    const finalPrice = parseFloat( ((mainPrice * priceCoefficient) + additionalService), 10);
-    const finalPriceEur = parseFloat( ((mainPrice * priceCoefficient) + additionalService), 10)/ 25.6;
-    const finalPriceEurWhole = parseInt(finalPriceEur, 10);
 
-    document.getElementById("formPrice").value = finalPrice+",- Kč or " + finalPriceEurWhole + ",- EUR";
+    const finalPriceCzk = parseFloat(((mainPrice) + additionalService), 10);
+
+    document.getElementById("formPrice").value = finalPriceCzk + ",- CZK";
 
     function getMainPrice() {
       const passTypePrice = passType.options[passType.selectedIndex].ariaValueNow;
       let mainPriceOutput = parseFloat(passTypePrice, 10);
-
-      const valueNotKnown = String(mainPriceOutput) == "NaN"; 
-
-      if (valueNotKnown) { //mostly for firefox browser
-        const selectedPriceIndex = passType.selectedIndex;
-        const backUpOption = document.querySelector(".priceBackup-option-" + String(selectedPriceIndex));
-        const backUpOptionPrice = backUpOption.innerText;
-        mainPriceOutput = backUpOptionPrice;
-      }
-
       return mainPriceOutput;
     }
-  
-    function getPriceCoefficient() {
-      const registrationTypeValue = parseFloat(registrationType.ariaPlaceholder, 10);
-      //const dancerKindValue = parseFloat(dancerKind.options[dancerKind.selectedIndex].ariaValueNow, 10);
-      //const lengthTypeValue = parseFloat(lengthType.options[lengthType.selectedIndex].ariaValueNow, 10);
 
-      //const priceCoefficientOutput = registrationTypeValue * dancerKindValue * lengthTypeValue;
-      let priceCoefficientOutput = registrationTypeValue;
-
-      const valueNotKnown = String(priceCoefficientOutput) == "NaN"; 
-
-      if (valueNotKnown){
-        const registrationTypePriceBackupValue = document.querySelector(".registrationTypePriceBackup").innerText;
-        priceCoefficientOutput = registrationTypePriceBackupValue;
-      }
-
-      return priceCoefficientOutput;
-    }
-  
     function getAdditionalService() {
-      const competitionParticipationValue = competitionParticipation.value;
-      let competitionParticipationOutput = parseFloat(competitionParticipation.options[competitionParticipation.selectedIndex].ariaValueNow, 10);
 
-      const valueNotKnownA = String(competitionParticipationOutput) == "NaN"; 
+      let otherTicketOptionsOutput = parseFloat(otherTicketOptions.options[otherTicketOptions.selectedIndex].ariaValueNow, 10);
+
+      const valueNotKnownA = String(otherTicketOptionsOutput) == "NaN";
 
       if (valueNotKnownA) {
         const selectorA = ".competitionPriceBackup-option-" + String(competitionParticipation.selectedIndex);
         const competitionBackUpValue = document.querySelector(selectorA).innerText;
-        competitionParticipationOutput = competitionBackUpValue;
+        otherTicketOptionsOutput = competitionBackUpValue;
       }
 
-      const merchandiseValue = merchandise.value;
-      let merchandiseOutput = parseFloat( merchandise.options[merchandise.selectedIndex].ariaValueNow, 10);
-
-      const valueNotKnownB = String(merchandiseOutput) == "NaN"; 
-
-      if (valueNotKnownB) {
-        const selectorB = ".merchandisePriceBackup-option-" + String(merchandise.selectedIndex);
-        const merchandiseBackUpValue = document.querySelector(selectorB).innerText;
-        merchandiseOutput = merchandiseBackUpValue;
-      }
-
-  
-      const sumAmount = parseFloat(competitionParticipationOutput, 10) + parseFloat(merchandiseOutput, 10);
-      
-      return sumAmount;
+      return otherTicketOptionsOutput;
     }
 
   } else {
