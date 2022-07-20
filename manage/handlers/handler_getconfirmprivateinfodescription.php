6644513b1bb75a1cad43a5725fb07222adcf933e
SimonB87 <buryans@gmail.com>
2022-07-20 13:19:26 +0200

    get confirmation description texts

---
+++ manage/handlers/handler_getconfirmprivateinfodescription.php
@@ -0,0 +1,58 @@
+<?php
+
+$confirmPrivateInformation1Description = "string";
+$confirmPrivateInformation2Description = "string";
+$confirmPrivateInformation3Description = "string";
+
+if (mysqli_connect_errno()) {
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+}
+
+/* debug
+if (!mysqli_query($connector, "SET a=1")) {
+  printf("Error message: %s\n", mysqli_error($link));
+}*/
+
+if (!mysqli_set_charset($connector, "utf8")) {
+  printf("Error loading character set utf8: %s\n", mysqli_error($con));
+  exit();
+  } else {
+          //printf("Current character set: %s\n", mysqli_character_set_name($con));//used only for testing
+  }
+
+  $sql = "SELECT * FROM events WHERE id=(SELECT max(id) FROM events)";
+
+  $results = $connector-> query($sql);
+  //Error case
+  if (!$sql ) {
+    echo "Failed! <br> Error sql: " . mysql_error();
+  }
+  
+  if (mysqli_query($connector, $sql)) {
+    //debug echo json_encode(array("statusCode"=>200));
+  } 
+  else {
+    echo json_encode(array("sql - statusCode"=>418));
+  }
+
+  //function to fatch the data
+  if ($results-> num_rows > 0 ) {
+    while ($row = $results-> fetch_assoc()) {
+
+      $eventDataId = $row["id"] ;
+      $confirmPrivateInformation1Description = $row["agreement1Text"] ;
+      $confirmPrivateInformation2Description = $row["agreement2Text"] ;
+      $confirmPrivateInformation3Description= $row["agreement3Text"] ;
+      
+    }
+    echo "";
+}
+else {
+    echo "There are 0 results in DB table";
+}
+
+// Comment out DB close
+// mysqli_close($connector);
+
+?>
