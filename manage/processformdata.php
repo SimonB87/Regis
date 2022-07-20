778ec44f5d387c6d40b36699b59e4c9a625e9343
SimonB87 <buryans@gmail.com>
2022-07-20 13:18:27 +0200

    process form data - corrected storing form registration data

--- manage/processformdata.php
+++ manage/processformdata.php
@@ -1,15 +1,16 @@
 <?php 
+include("handlers/handler_getconfirmprivateinfodescription.php");
 
 if ((isset($_POST['clientName'])) or (isset($_POST['clientEmail']))) {
 
+  $eventID = mysqli_real_escape_string($connector, $_POST['eventID']); //* ** */
   $eventName = mysqli_real_escape_string($connector, $_POST['eventName']);
   $passType = mysqli_real_escape_string($connector, $_POST['passType']);
-  $registrationType = mysqli_real_escape_string($connector, $_POST['registrationType']);
+  //$registrationType = mysqli_real_escape_string($connector, $_POST['registrationType']);
   $dancerKind = mysqli_real_escape_string($connector, $_POST['dancerKind']);
-  $lengthType = mysqli_real_escape_string($connector, $_POST['lengthType']);
-  $competitionParticipation = mysqli_real_escape_string($connector, $_POST['competitionParticipation']);
+  $otherTicketOptions = mysqli_real_escape_string($connector, $_POST['otherTicketOptions']); /* ** */
+  //$competitionParticipation = mysqli_real_escape_string($connector, $_POST['competitionParticipation']);
   $location = mysqli_real_escape_string($connector, $_POST['location']);
-  $merchandise = mysqli_real_escape_string($connector, $_POST['merchandise']);
   $formPrice = mysqli_real_escape_string($connector, $_POST['formPrice']);
   $clientName = mysqli_real_escape_string($connector, $_POST['clientName']);
   $clientEmail = mysqli_real_escape_string($connector, $_POST['clientEmail']);
@@ -17,10 +18,14 @@ if ((isset($_POST['clientName'])) or (isset($_POST['clientEmail']))) {
   $clientCountry = mysqli_real_escape_string($connector, $_POST['clientCountry']);
   $clientComments = mysqli_real_escape_string($connector, $_POST['clientComments']);
   $registrationdate = mysqli_real_escape_string($connector, $_POST['registrationdate']);
-  $confirmPrivateInformation = mysqli_real_escape_string($connector, $_POST['confirmPrivateInformation']);
-  $confirmCovid = mysqli_real_escape_string($connector, $_POST['confirmCovid']);
-  $confirmCovidResult = ($confirmCovid == "on") ? "Yes, agreed with COVID-19 therms" : "Denied COVID-19 therms." ;
-  $confirmPrivateInformationResult = ($confirmPrivateInformation == "on") ? "Yes, agreed with GDPR therms" : "Denied GDPR therms." ;
+
+  $confirmPrivateInformation1 = isset($_POST['confirmPrivateInformation1']) ? mysqli_real_escape_string($connector, $_POST['confirmPrivateInformation1']) : null;
+  $confirmPrivateInformation2 = isset($_POST['confirmPrivateInformation2']) ? mysqli_real_escape_string($connector, $_POST['confirmPrivateInformation2']) : null;
+  $confirmPrivateInformation3 = isset($_POST['confirmPrivateInformation3']) ? mysqli_real_escape_string($connector, $_POST['confirmPrivateInformation3']) : null;
+
+  $confirmPrivateInformation1 = ($confirmPrivateInformation1 == "on") ? "Yes" : $confirmPrivateInformation1;
+  $confirmPrivateInformation2 = ($confirmPrivateInformation2 == "on") ? "Yes" : $confirmPrivateInformation2;
+  $confirmPrivateInformation3 = ($confirmPrivateInformation3 == "on") ? "Yes" : $confirmPrivateInformation3;
   
   // form validation
   $validDancerKind = ($dancerKind == "") ? false : true;
@@ -49,9 +54,8 @@ if ((isset($_POST['clientName'])) or (isset($_POST['clientEmail']))) {
   
     if ($correctlyFilledForm){
   
-      $sql = "INSERT INTO `registrations`( `eventName`,`passType`,`registrationType`,`dancerKind`,`lengthType`,`competitionParticipation`,`location`,`merchandise`,`formPrice`,`clientName`,`clientPhone`,`clientCountry`, `clientEmail` ,`clientComments`,`registrationdate`,`confirmPrivateInformation`,`confirmCovid`) 
-      VALUES ('$eventName', '$passType ', '$registrationType', '$dancerKind', '$lengthType', '$competitionParticipation', '$location', '$merchandise', '$formPrice', '$clientName', '$clientPhone', '$clientCountry', '$clientEmail', '$clientComments', '$registrationdate', '$confirmPrivateInformationResult', '$confirmCovidResult')";
-      
+      $sql = "INSERT INTO `registrations`( `eventID`,`eventName`,`passType`,`dancerKind`,`otherTicketOptions`,`location`,`formPrice`,`clientName`,`clientPhone`,`clientCountry`, `clientEmail` ,`clientComments`,`registrationdate`,`confirmPrivateInformation1Description`,`confirmPrivateInformation1`,`confirmPrivateInformation2Description`,`confirmPrivateInformation2`,`confirmPrivateInformation3Description`,`confirmPrivateInformation3`) 
+      VALUES ('$eventID', '$eventName', '$passType ', '$dancerKind', '$otherTicketOptions', '$location', '$formPrice', '$clientName', '$clientPhone', '$clientCountry', '$clientEmail', '$clientComments', '$registrationdate', '$confirmPrivateInformation1Description', '$confirmPrivateInformation1', '$confirmPrivateInformation2Description', '$confirmPrivateInformation2', '$confirmPrivateInformation3Description', '$confirmPrivateInformation3')";
       
       //Error case
       if (!$sql ) {
