b292a3eee01d35a04f581539685993665662e6bf
SimonB87 <buryans@gmail.com>
2022-07-28 01:56:18 +0200

    prefil selected rgistrations kinds

--- shared/assets/js/editregistrations.js
+++ shared/assets/js/editregistrations.js
@@ -5,7 +5,7 @@ function editRegistration(orderNum) {
   const orderRowSelector = "#orders tr.orderId" + orderNum;
 
   const orderDancerKindVal = document.querySelector(orderRowSelector + " td[title=dancerKind]").innerText;
-  const orderPassTypeVal = document.querySelector(orderRowSelector + " td[title = passType]").innerText;
+  const orderPassTypeVal = document.querySelector(orderRowSelector + " td[title=passType]").innerText;
   const orderOtherTicketOptionsVal = document.querySelector(orderRowSelector + " td[title=otherTicketOptions]").innerText;
   const orderClientNameVal = document.querySelector(orderRowSelector + " td[title=clientName]").innerText;
   const orderClientEmailVal = document.querySelector(orderRowSelector + " td[title=clientEmail]").innerText;
@@ -13,10 +13,31 @@ function editRegistration(orderNum) {
   const orderClientCountryVal = document.querySelector(orderRowSelector + " td[title=clientCountry]").innerText;
   const orderClientCommentsVal = document.querySelector(orderRowSelector + " td[title=clientComments]").innerText;
 
+  if (orderDancerKindVal.length > 1) {
+    selectOptions(orderDancerKindVal, "dancerKind");
+  }
+
+  if (orderPassTypeVal.length > 1) {
+    selectOptions(orderPassTypeVal, "passType");
+  }
+
+  if (orderOtherTicketOptionsVal.length > 1) {
+    selectOptions(orderOtherTicketOptionsVal, "otherTicketOptions");
+  }
+
   document.getElementById("clientName").value = orderClientNameVal;
   document.getElementById("clientEmail").value = orderClientEmailVal;
   document.getElementById("clientPhone").value = orderClientPhoneVal;
   document.getElementById("clientCountry").value = orderClientCountryVal;
   document.getElementById("clientComments").innerText = orderClientCommentsVal;
+
+  function selectOptions(valueOfSource, targetSelectId) {
+    const selectOptions = document.querySelectorAll("#" + targetSelectId + " option");
+    selectOptions.forEach((element, index) => {
+      if (element.value[0] == valueOfSource[0]) {
+        selectOptions[index].setAttribute("selected", "");
+      }
+    });
+  }
 
 }
