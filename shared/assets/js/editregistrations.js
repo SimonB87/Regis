345d9cad8286af8e33f1a2282a285828b35c5dd2
SimonB87 <buryans@gmail.com>
2022-07-27 22:29:01 +0200

    JS - Edit Registration

--- shared/assets/js/editregistrations.js
+++ shared/assets/js/editregistrations.js
@@ -1,4 +1,22 @@
 function editRegistration(orderNum) {
-  const target = document.getElementById("orderId");
-  target.innerText = orderNum;
+  const targetOrderIdHeading = document.getElementById("orderId");
+  targetOrderIdHeading.innerText = orderNum;
+
+  const orderRowSelector = "#orders tr.orderId" + orderNum;
+
+  const orderDancerKindVal = document.querySelector(orderRowSelector + " td[title=dancerKind]").innerText;
+  const orderPassTypeVal = document.querySelector(orderRowSelector + " td[title = passType]").innerText;
+  const orderOtherTicketOptionsVal = document.querySelector(orderRowSelector + " td[title=otherTicketOptions]").innerText;
+  const orderClientNameVal = document.querySelector(orderRowSelector + " td[title=clientName]").innerText;
+  const orderClientEmailVal = document.querySelector(orderRowSelector + " td[title=clientEmail]").innerText;
+  const orderClientPhoneVal = document.querySelector(orderRowSelector + " td[title=clientPhone]").innerText;
+  const orderClientCountryVal = document.querySelector(orderRowSelector + " td[title=clientCountry]").innerText;
+  const orderClientCommentsVal = document.querySelector(orderRowSelector + " td[title=clientComments]").innerText;
+
+  document.getElementById("clientName").value = orderClientNameVal;
+  document.getElementById("clientEmail").value = orderClientEmailVal;
+  document.getElementById("clientPhone").value = orderClientPhoneVal;
+  document.getElementById("clientCountry").value = orderClientCountryVal;
+  document.getElementById("clientComments").innerText = orderClientCommentsVal;
+
 }
