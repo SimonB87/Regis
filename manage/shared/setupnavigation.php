35aea4fadbc3f4d641960b47c9e6dc527ce33649
SimonB87 <buryans@gmail.com>
2022-08-07 11:33:26 +0200

    setup navigation - edited background

--- manage/shared/setupnavigation.php
+++ manage/shared/setupnavigation.php
@@ -1,35 +1,39 @@
-<div class="row padding-regular rounded-3 bg-light border">
+<div class="row padding-regular-top">
 
-  <div class="col-12 text-center">
-    <span> Event set-up links : </span>
-  </div>
+  <div class="border padding-regular bg-light rounded-3 bg-light border">
 
-  <?php 
-  $urlpath = $_SERVER['REQUEST_URI'];
-  $isModeEventCreate = strpos($urlpath, "createnewevent");
-?>
+    <div class="col-12 text-center">
+      <span> Event set-up links : </span>
+    </div>
 
-  <div class="col-12 text-center">
+    <?php 
+    $urlpath = $_SERVER['REQUEST_URI'];
+    $isModeEventCreate = strpos($urlpath, "createnewevent");
+    ?>
 
-    <a id="nav_eventsetupmain"
-      href=<?php if($isModeEventCreate) {echo "createneweventsetupmain.php"; } else {echo "eventsetupmain.php"; } ?>
-      class="weblink padding-small "> Event main set up </a>
+    <div class="col-12 text-center">
 
-    <a id="nav_eventdescription"
-      href=<?php if($isModeEventCreate) {echo "createneweventeventdescription.php"; } else {echo "eventdescription.php"; } ?>
-      class="weblink padding-small"> Event description </a>
+      <a id="nav_eventsetupmain"
+        href=<?php if($isModeEventCreate) {echo "createneweventsetupmain.php"; } else {echo "eventsetupmain.php"; } ?>
+        class="weblink padding-small "> Event main set up </a>
 
-    <a id="nav_eventposter" href="eventposter.php" class="weblink padding-small"> Event poster </a>
-<!--
-    <a id="nav_merchandise" href="merchandisesetup.php" class="weblink padding-small"> Merchendase set-up </a>
--->
-    <a id="nav_eventform"
-      href=<?php if($isModeEventCreate) {echo "createneweventeventformpreview.php"; } else {echo "eventformpreview.php"; } ?>
-      class="weblink padding-small"> Event register form </a>
+      <a id="nav_eventdescription"
+        href=<?php if($isModeEventCreate) {echo "createneweventeventdescription.php"; } else {echo "eventdescription.php"; } ?>
+        class="weblink padding-small"> Event description </a>
+
+      <a id="nav_eventposter" href="eventposter.php" class="weblink padding-small"> Event poster </a>
+  <!--
+      <a id="nav_merchandise" href="merchandisesetup.php" class="weblink padding-small"> Merchendase set-up </a>
+  -->
+      <a id="nav_eventform"
+        href=<?php if($isModeEventCreate) {echo "createneweventeventformpreview.php"; } else {echo "eventformpreview.php"; } ?>
+        class="weblink padding-small"> Event register form </a>
 
 
 
-    <a id="nav_eventform" href="publishevent.php" class="weblink padding-small"> Publish event (leave event setup) </a>
+      <a id="nav_eventform" href="publishevent.php" class="weblink padding-small"> Publish event (leave event setup) </a>
+    </div>
+
   </div>
 
 </div>
