eba577544d1f8d77ae653deffc558a2742d4063e
SimonB87 <buryans@gmail.com>
2022-08-07 22:46:27 +0200

    db storage

--- development/events.sql
+++ development/events.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Ned 31. čec 2022, 13:49
--- Verze serveru: 10.4.14-MariaDB
--- Verze PHP: 7.4.9
+-- Vytvořeno: Ned 07. srp 2022, 21:52
+-- Verze serveru: 10.1.30-MariaDB
+-- Verze PHP: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `events2`
+-- Struktura tabulky `events`
 --
 
-CREATE TABLE `events2` (
+CREATE TABLE `events` (
   `id` int(250) NOT NULL,
   `eventStatus` varchar(250) CHARACTER SET latin1 NOT NULL,
   `eventName` longtext CHARACTER SET latin1 NOT NULL,
@@ -74,9 +75,9 @@ CREATE TABLE `events2` (
   `partyTicketAmountSingle` int(250) DEFAULT NULL,
   `partyTicketPriceCouple` int(250) DEFAULT NULL,
   `partyTicketAmountCouple` int(250) DEFAULT NULL,
-  `eventDescription1` longtext CHARACTER SET latin1 DEFAULT NULL,
-  `eventDescription2` longtext CHARACTER SET latin1 DEFAULT NULL,
-  `eventDescription3` longtext CHARACTER SET latin1 DEFAULT NULL,
+  `eventDescription1` longtext CHARACTER SET latin1,
+  `eventDescription2` longtext CHARACTER SET latin1,
+  `eventDescription3` longtext CHARACTER SET latin1,
   `agreementsEnabled` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
   `agreement1Enabled` varchar(10) CHARACTER SET latin1 DEFAULT NULL,
   `agreement1Text` varchar(250) CHARACTER SET latin1 DEFAULT NULL,
@@ -88,21 +89,22 @@ CREATE TABLE `events2` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Vypisuji data pro tabulku `events2`
+-- Vypisuji data pro tabulku `events`
 --
 
-INSERT INTO `events2` (`id`, `eventStatus`, `eventName`, `eventStartDate`, `eventEndDate`, `maintainSinglesParity`, `earlyBirdsRegistrationEnabled`, `earlyBirdsRegistrationName`, `earlyBirdsRegistrationsStartDate`, `earlyBirdsRegistrationsEndDate`, `ticketsAmountEarlyBirdsRegistrationsSingle`, `earlyBirdsTicketPriceSingle`, `earlyBirdsTicketPriceCouple`, `earlyBirdsTicketAmountCouple`, `regularRegistrationEnabled`, `regularRegistrationName`, `regularRegistrationsStartDate`, `regularRegistrationsEndDate`, `regularTicketPriceSingle`, `regularTicketAmountSingle`, `regularTicketPriceCouple`, `regularTicketAmountCouple`, `specialType1RegistrationEnabled`, `specialType1RegistrationName`, `specialType1RegistrationsStartDate`, `specialType1RegistrationsEndDate`, `specialType1TicketPriceSingle`, `specialType1TicketAmountSingle`, `specialType1TicketPriceCouple`, `specialType1TicketAmountCouple`, `specialType2RegistrationEnabled`, `specialType2RegistrationName`, `specialType2RegistrationsStartDate`, `specialType2RegistrationsEndDate`, `specialType2TicketPriceSingle`, `specialType2TicketAmountSingle`, `specialType2TicketPriceCouple`, `specialType2TicketAmountCouple`, `partyRegistrationEnabled`, `partyRegistrationName`, `partyRegistrationsStartDate`, `partyRegistrationsEndDate`, `partyTicketPriceSingle`, `partyTicketAmountSingle`, `partyTicketPriceCouple`, `partyTicketAmountCouple`, `eventDescription1`, `eventDescription2`, `eventDescription3`, `agreementsEnabled`, `agreement1Enabled`, `agreement1Text`, `agreement2Enabled`, `agreement2Text`, `agreement3Enabled`, `agreement3Text`, `posterFileName`) VALUES
+INSERT INTO `events` (`id`, `eventStatus`, `eventName`, `eventStartDate`, `eventEndDate`, `maintainSinglesParity`, `earlyBirdsRegistrationEnabled`, `earlyBirdsRegistrationName`, `earlyBirdsRegistrationsStartDate`, `earlyBirdsRegistrationsEndDate`, `ticketsAmountEarlyBirdsRegistrationsSingle`, `earlyBirdsTicketPriceSingle`, `earlyBirdsTicketPriceCouple`, `earlyBirdsTicketAmountCouple`, `regularRegistrationEnabled`, `regularRegistrationName`, `regularRegistrationsStartDate`, `regularRegistrationsEndDate`, `regularTicketPriceSingle`, `regularTicketAmountSingle`, `regularTicketPriceCouple`, `regularTicketAmountCouple`, `specialType1RegistrationEnabled`, `specialType1RegistrationName`, `specialType1RegistrationsStartDate`, `specialType1RegistrationsEndDate`, `specialType1TicketPriceSingle`, `specialType1TicketAmountSingle`, `specialType1TicketPriceCouple`, `specialType1TicketAmountCouple`, `specialType2RegistrationEnabled`, `specialType2RegistrationName`, `specialType2RegistrationsStartDate`, `specialType2RegistrationsEndDate`, `specialType2TicketPriceSingle`, `specialType2TicketAmountSingle`, `specialType2TicketPriceCouple`, `specialType2TicketAmountCouple`, `partyRegistrationEnabled`, `partyRegistrationName`, `partyRegistrationsStartDate`, `partyRegistrationsEndDate`, `partyTicketPriceSingle`, `partyTicketAmountSingle`, `partyTicketPriceCouple`, `partyTicketAmountCouple`, `eventDescription1`, `eventDescription2`, `eventDescription3`, `agreementsEnabled`, `agreement1Enabled`, `agreement1Text`, `agreement2Enabled`, `agreement2Text`, `agreement3Enabled`, `agreement3Text`, `posterFileName`) VALUES
 (1, '0 - Registrations closed', 'Alfa Prime X', '2022-02-28', '2022-03-01', 'on', 'on', 'The Early Birds Registrations', '2022-02-21', '2022-02-22', 1000, 1000, 1900, 10, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, ''),
-(2, '1 - Registrations open', 'Demo Generation One Vol. 1', '2022-09-02', '2022-09-04', 'Yes', 'Yes', 'Demo ACigknYOoE', '2022-07-25', '2022-08-07', 4, 900, 1600, 2, 'Yes', 'Demo AYvujAuoEa', '2022-07-19', '2022-09-30', 1000, 4, 1800, 2, 'Yes', 'Jack and Jill competition', '2022-07-01', '2022-09-07', 20, 500, 3, 950, 'Yes', 'Master Class', '2022-07-25', '2022-09-05', 500, 10, 700, 4, 'Yes', 'Demo AOnXZKYRCG', '2022-07-19', '2022-08-07', 500, 4, 900, 2, ' Famous Tester & Testie for the 1st time in Prague! Experience beautiful Easter Prague and feel that the Bachata love is all around you ?\r\n? Exclusive 2-day workshop of romantic Bachata with Tester & Testie, special workshop Bachata connection principles with Mauritius & Elvira, pre-party workshops\r\n? Parties with DJ MomoLatino, Tester & Testie and other artists\r\n? Jack & Jill (max. 6 leaders, 6 followers), 1st price: private lesson with Tester & Testie!\r\n? Possibility to arrange private lessons (both Tester & Testie will be present for the couple)\r\n', '? Venue:\r\nTresor Club Prague, Vinohradská 25, Prague\r\n(pre-party in Dancer\'s Club, Kon?vova 929)\r\n', '? Prices:\r\nFullpass: SOLD OUT !!!\r\nPossibility to go on the waiting list. In case some dancer will not be able to participate, we will contact you. Let us know.\r\n\r\nParty Pass: 450 CZK / 18 euro\r\nJack & Jill starting fee (max. 6 leaders, 6 followers): 250 CZK / 10 euro for Fullpass holders, 500 CZK / 20 euro for others', 'Yes', 'Yes', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'Yes', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', 'No', '', 'uploads/poster_janis_zoe.jpg');
+(2, '2 - Event passed', 'Demo Generation One Vol. 1', '2022-09-02', '2022-09-04', 'Yes', 'Yes', 'Demo ACigknYOoE', '2022-07-25', '2022-08-07', 4, 900, 1600, 2, 'Yes', 'Demo AYvujAuoEa', '2022-07-19', '2022-09-30', 1000, 4, 1800, 2, 'Yes', 'Jack and Jill competition', '2022-07-01', '2022-09-07', 20, 500, 3, 950, 'Yes', 'Master Class', '2022-07-25', '2022-09-05', 500, 10, 700, 4, 'Yes', 'Demo AOnXZKYRCG', '2022-07-19', '2022-08-07', 500, 4, 900, 2, ' Famous Tester & Testie for the 1st time in Prague! Experience beautiful Easter Prague and feel that the Bachata love is all around you ?\r\n? Exclusive 2-day workshop of romantic Bachata with Tester & Testie, special workshop Bachata connection principles with Mauritius & Elvira, pre-party workshops\r\n? Parties with DJ MomoLatino, Tester & Testie and other artists\r\n? Jack & Jill (max. 6 leaders, 6 followers), 1st price: private lesson with Tester & Testie!\r\n? Possibility to arrange private lessons (both Tester & Testie will be present for the couple)\r\n', '? Venue:\r\nTresor Club Prague, Vinohradská 25, Prague\r\n(pre-party in Dancer\'s Club, Kon?vova 929)\r\n', '? Prices:\r\nFullpass: SOLD OUT !!!\r\nPossibility to go on the waiting list. In case some dancer will not be able to participate, we will contact you. Let us know.\r\n\r\nParty Pass: 450 CZK / 18 euro\r\nJack & Jill starting fee (max. 6 leaders, 6 followers): 250 CZK / 10 euro for Fullpass holders, 500 CZK / 20 euro for others', 'Yes', 'Yes', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'Yes', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', 'No', '', 'uploads/poster_janis_zoe.jpg'),
+(3, '1 - Registrations open', 'Sensual Weekend - Bachata workshop', '2022-12-02', '2022-12-03', 'Yes', 'Yes', 'Early birds', '2022-08-05', '2022-11-30', 6, 2500, 4700, 3, 'Yes', 'Full pass', '2022-08-05', '2022-11-30', 3000, 6, 5500, 3, 'Yes', 'Master class', '2022-08-05', '2022-11-30', 750, 2, 1200, 2, 'No', '', '', '', 0, 0, 0, 0, 'Yes', 'Party pass', '2022-08-05', '2022-11-30', 1900, 4, 3500, 2, 'Artists\r\n????\r\n? Melvin y Gatica, Bachata Influence - for the 1st time in Prague!\r\n? Igor y Rocio, Bachata Feeling - after 2021 big success again in Prague!\r\n? DJ MomoLatino, international DJ, Italy\r\n', 'What awaits you?\r\n????????\r\n? 8 hours of main workshops with 4 artists (2 levels of workshops, happening simultaneously)\r\n? 2 special Lady styling lessons with Gatica\r\n? Gender Bachata battle pre-party class with Sorush & Greta\r\n? 2 rooftop parties\r\n? Main party in legendary Tresor Club with DJ Tony Pecino and DJ MomoLatino\r\n Types of tickets\r\n Fullpass advanced (30-40 couples, 8 hours of advanced lessons - 3 hours with Melvin-Gatica, 3 hours with Igor-Rocio, 60 min with Sorush-Greta, 60 min with Mauritius-Elvira, 3 parties, Gender Bachata battle with Sorush y Grete)\r\n Fullpass classic (30-40 couples, 8 hours of intermediate-advanced lessons - 3 hours with Melvin-Gatica, 3 hours with Igor-Rocio, 60 min with Sorush-Greta, 60 min with Mauritius-Elvira, 3 parties, Gender Bachata battle with Sorush y Grete)\r\n', 'FAQ - Frequently asked questions\r\n???????????????\r\n? What is the difference between Group 1 (Advanced) and Group 2 (classical Fullpass)?\r\n Main difference is the level, Group 1 being only advanced+, group 2 something between intermediate-advanced. Workshops in both groups will be happening simultaneously. Both groups will have 8 hours of workshops. The basic content of workshops will be the same, only it will be suited according to the level of the dancers. We have chosen idea of 2 separate groups of different levels to make the learning as effective as possible for everybody, it\'s the new concept. In case somebody will not be suitable for either of the level, Artists may suggest moving the person to the other group.\r\n? What if I will find out that I am not a good fit for the level group?\r\n In that case talk to us and we will try to find a solution.\r\n? What if somebody else will suggest different level for me?\r\n In case some of the artists would notice that you are not a good fit for the level that you have bought, then you will be asked to switch to the other group.\r\n? If I will be moved to the other group based on the recommendation of the Artists, will I get refund of a price difference?\r\n', 'Yes', 'Yes', 'I confirm therms of sevice', 'Yes', 'I confirm GDPR therms', 'No', '', 'uploads/melvin_june23.jpg');
 
 --
 -- Klíče pro exportované tabulky
 --
 
 --
--- Klíče pro tabulku `events2`
+-- Klíče pro tabulku `events`
 --
-ALTER TABLE `events2`
+ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -110,10 +112,10 @@ ALTER TABLE `events2`
 --
 
 --
--- AUTO_INCREMENT pro tabulku `events2`
+-- AUTO_INCREMENT pro tabulku `events`
 --
-ALTER TABLE `events2`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `events`
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- development/registrations.sql
+++ development/registrations.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Ned 31. čec 2022, 13:49
--- Verze serveru: 10.4.14-MariaDB
--- Verze PHP: 7.4.9
+-- Vytvořeno: Ned 07. srp 2022, 21:53
+-- Verze serveru: 10.1.30-MariaDB
+-- Verze PHP: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `registrations2`
+-- Struktura tabulky `registrations`
 --
 
-CREATE TABLE `registrations2` (
+CREATE TABLE `registrations` (
   `id` int(255) NOT NULL,
   `orderID` varchar(255) CHARACTER SET latin2 COLLATE latin2_czech_cs NOT NULL,
   `eventID` int(255) NOT NULL,
@@ -58,13 +59,13 @@ CREATE TABLE `registrations2` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Vypisuji data pro tabulku `registrations2`
+-- Vypisuji data pro tabulku `registrations`
 --
 
-INSERT INTO `registrations2` (`id`, `orderID`, `eventID`, `eventName`, `clientName`, `passType`, `registrationType`, `dancerKind`, `otherTicketOptions`, `competitionParticipation`, `location`, `merchandise`, `formPrice`, `clientEmail`, `clientPhone`, `clientCountry`, `clientComments`, `registrationdate`, `confirmPrivateInformation1`, `confirmPrivateInformation2`, `confirmPrivateInformation3`, `confirmPrivateInformation1Description`, `confirmPrivateInformation2Description`, `confirmPrivateInformation3Description`, `paystatus`, `clientTransferedOrder`, `adminEditedOrder`) VALUES
-(105, '20220105', 0, 'Demo Generation One Vol. 1', 'Simon Test 1', '1 - Early birds pass leader/follower ', '', '1 - Leader', '', '0 - None', 'Prague', '', '... ,- Kč', 'simon@test.eu', '+420777000333', 'CZ', 'Test Alfa 1', 'Datum: 19. 07. 2022 - 21:45:11', 'Yes, agreed with GDPR therms', 'Denied COVID-19 therms.', '', '', '', '', '', '', ''),
-(125, '20220125', 2, 'Demo Generation One Vol. 1', 'Simon Test 9', '2 - Early birds couple ', '', '1 - Leader', '0 - None', '', 'Prague', '', '1600,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 12:42:55', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '', '', ''),
-(128, '20220128', 2, 'Demo Generation One Vol. 1', 'Simon Test 2', '5 - Partypass leader/follower ', '', '1 - Leader', '0 - None', '', 'Prague', '', '500,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 12:57:0', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '3 - reminder sent', '', ''),
+INSERT INTO `registrations` (`id`, `orderID`, `eventID`, `eventName`, `clientName`, `passType`, `registrationType`, `dancerKind`, `otherTicketOptions`, `competitionParticipation`, `location`, `merchandise`, `formPrice`, `clientEmail`, `clientPhone`, `clientCountry`, `clientComments`, `registrationdate`, `confirmPrivateInformation1`, `confirmPrivateInformation2`, `confirmPrivateInformation3`, `confirmPrivateInformation1Description`, `confirmPrivateInformation2Description`, `confirmPrivateInformation3Description`, `paystatus`, `clientTransferedOrder`, `adminEditedOrder`) VALUES
+(105, '20220105', 1, 'Alfa Prime X', 'Simon Test 1', '1 - Early birds pass leader/follower ', '', '1 - Leader', '', '0 - None', 'Prague', '', '1600,- CZK 	', 'simon@test.eu', '+420777000333', 'CZ', 'Test Alfa 1', 'Datum: 19. 07. 2022 - 21:45:11', 'Yes, agreed with GDPR therms', 'Denied COVID-19 therms.', '', '', '', '', '', '', ''),
+(125, '20220125', 1, 'Alfa Prime X', 'Simon Test 9', '2 - Early birds couple ', '', '1 - Leader', '0 - None', '', 'Prague', '', '1600,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 12:42:55', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '', '', ''),
+(128, '20220128', 1, 'Alfa Prime X', 'Simon Test 2', '5 - Partypass leader/follower ', '', '1 - Leader', '0 - None', '', 'Prague', '', '500,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 12:57:0', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '3 - reminder sent', '', ''),
 (129, '20220129', 2, 'Demo Generation One Vol. 1', 'Simon Test 2', '1 - Early birds pass leader/follower ', '', '1 - Leader', '0 - None', '', 'Prague', '', '900,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 13:01:37', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '2 - paid', '', ''),
 (130, '20220130', 2, 'Demo Generation One Vol. 1', 'Simon Test 2', '6 - Partypass couple ', '', '1 - Leader', '0 - None', '', 'Prague', '', '900,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'Alfa 1', 'Datum: 20. 07. 2022 - 13:15:0', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '2 - paid', '', ''),
 (131, '20220131', 2, 'Demo Generation One Vol. 1', 'Simon Test 2', '3 - Fullpass leader/follower ', '', '1 - Leader', '0 - None', '', 'Prague', '', '1000,- CZK', 'simon@test.eu', '+420777000333', 'CZ', 'comemnts ', 'Datum: 25. 07. 2022 - 15:51:27', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '2 - paid', '', ''),
@@ -83,16 +84,23 @@ INSERT INTO `registrations2` (`id`, `orderID`, `eventID`, `eventName`, `clientNa
 (145, '20220145', 2, 'Demo Generation One Vol. 1', 'Simon Test Couple 28.07-0004', '6 - Partypass couple', '', '3 - Couple', '2 - Jack and Jill competition', '', 'Prague', '', '1000,- CZK', 'simontest@test0004.eu', '+4202807000004', 'CZ', 'Edited comments Edit order ID : 20220145, 0004', 'Datum: 25. 07. 2022 - 15:51:27', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '2 - paid', '', ' edited order data 28.07.2022;'),
 (146, '20220146', 2, 'Demo Generation One Vol. 1', 'Simon 146 Test Follower 0005', '6 - Partypass couple', '', '3 - Couple', '4 - Master Class', '', 'Prague', '', '1600,- CZK', 'simonFollower005@test.eu', '+4201460005', 'DE0005', '', 'Datum: 28. 07. 2022 - 13:50:52', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '1 - unpaid', '', ''),
 (147, '20220145', 2, 'Demo Generation One Vol. 1', 'Simon Test Couple 28.07-0004', '6 - Partypass couple', '', '3 - Couple', '2 - Jack and Jill competition', '', 'Prague', '', '1000,- CZK', 'simontest@test0004.eu', '+4202807000004', 'CZ', 'Edited comments Edit order ID : 20220145, 0004', 'Datum: 25. 07. 2022 - 15:51:27', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '2 - paid', '', ' edited order data 28.07.2022;'),
-(148, '20220148', 2, 'Demo Generation One Vol. 1', 'Štepánka Testovací', '4 - Fullpass couple ', '', '2 - Follower', '4 - Master Class - 700,- Kč', '', 'Prague', '', '2500,- CZK', 'test@stepa2807.cz', '+420777000898', 'SK', 'Štepánka Testovací Customer contact 28.07.2022 19:50', 'Datum: 28. 07. 2022 - 19:48:45', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '1 - unpaid', 'Original Order - Datum: 28. 07. 2022 - 19:48:45;', 'Original Order - Datum: 28. 07. 2022 - 19:48:45;');
+(148, '20220148', 2, 'Demo Generation One Vol. 1', 'Štepánka Testovací', '4 - Fullpass couple ', '', '2 - Follower', '4 - Master Class - 700,- Kč', '', 'Prague', '', '2500,- CZK', 'test@stepa2807.cz', '+420777000898', 'SK', 'Štepánka Testovací Customer contact 28.07.2022 19:50', 'Datum: 28. 07. 2022 - 19:48:45', 'Yes', 'Yes', '', 'I agree with storing the information I am providing you and I agree with your company GDP policy/therms GDP policy/therms', 'I am vaccinated against COVID-19 or I have valid COVID-19 test COVID-19 policy', '', '1 - unpaid', 'Original Order - Datum: 28. 07. 2022 - 19:48:45;', 'Original Order - Datum: 28. 07. 2022 - 19:48:45;'),
+(149, '20220149', 3, 'Sensual Weekend - Bachata workshop', 'Stepanka 1', '3 - Fullpass leader/follower ', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '+4207777', 'Czechia', 'b drhd hrdh djh', 'Datum: 06. 08. 2022 - 23:15:23', 'Yes', 'Yes', '', 'I confirm orginizers\' therms of service.', 'I confirm orginizers\' GDPR therms. Organizer will store mz personal data for event admission purpose.', '', '2 - paid', 'Original Order - Datum: 06. 08. 2022 - 23:15:23;', ' edited paystatus 06.08.2022;'),
+(150, '20220150', 3, 'Sensual Weekend - Bachata workshop', 'Stepanka 2', '3 - Fullpass leader/follower ', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '+4207777', 'Czechia', 'alwafp fqofn fqflpmn rvbů§m', 'Datum: 06. 08. 2022 - 23:21:25', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '2 - paid', 'Original Order - Datum: 06. 08. 2022 - 23:21:25;', ' edited paystatus 06.08.2022;'),
+(151, '20220151', 3, 'Sensual Weekend - Bachata workshop', 'Stepnka 4', '3 - Fullpass leader/follower ', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '+4207777', 'Czechia', 'fajk fafmn gveasm bs bsroúm', 'Datum: 07. 08. 2022 - 12:27:24', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '1 - unpaid', 'Original Order - Datum: 07. 08. 2022 - 12:27:24;', 'Original Order - Datum: 07. 08. 2022 - 12:27:24;'),
+(152, '20220152', 3, 'Sensual Weekend - Bachata workshop', 'Stepanie 3', '3 - Fullpass leader/follower ', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '732716030', 'Czechia', 'sgnmp rgomg rúm ůsrml gbú sngiéav', 'Datum: 07. 08. 2022 - 12:29:24', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '1 - unpaid', 'Original Order - Datum: 07. 08. 2022 - 12:29:24;', 'Original Order - Datum: 07. 08. 2022 - 12:29:24;'),
+(153, '20220153', 3, 'Sensual Weekend - Bachata workshop', 'Stepanie 5', '3 - Fullpass leader/follower ', '', '2 - Follower', '1 - Master class - 750,- Kč', '', 'Prague', '', '3750,- CZK', 'sim@on.cz', '+420777', 'Czechia', 'h d  tjh', 'Datum: 07. 08. 2022 - 12:30:20', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '1 - unpaid', 'Original Order - Datum: 07. 08. 2022 - 12:30:20;', 'Original Order - Datum: 07. 08. 2022 - 12:30:20;'),
+(154, '20220154', 3, 'Sensual Weekend - Bachata workshop', 'Stepanie 6', '3 - Fullpass leader/follower ', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '+420777', 'Czechia', 'gsgsg hh fgz', 'Datum: 07. 08. 2022 - 15:19:51', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '1 - unpaid', 'Original Order - Datum: 07. 08. 2022 - 15:19:51;', 'Original Order - Datum: 07. 08. 2022 - 15:19:51;'),
+(155, '20220155', 3, 'Sensual Weekend - Bachata workshop', 'Stepanie 7', '5 - Partypass leader/follower', '', '2 - Follower', '0 - None', '', 'Prague', '', '3000,- CZK', 'sim@on.cz', '+420777000111', 'Czechia', 'zfg  dfg', 'Datum: 07. 08. 2022 - 15:21:58', 'Yes', 'Yes', '', 'I confirm therms of sevice', 'I confirm GDPR therms', '', '1 - unpaid', 'Original Order - Datum: 07. 08. 2022 - 15:21:58;', ' edited order data 07.08.2022;');
 
 --
 -- Klíče pro exportované tabulky
 --
 
 --
--- Klíče pro tabulku `registrations2`
+-- Klíče pro tabulku `registrations`
 --
-ALTER TABLE `registrations2`
+ALTER TABLE `registrations`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -100,10 +108,10 @@ ALTER TABLE `registrations2`
 --
 
 --
--- AUTO_INCREMENT pro tabulku `registrations2`
+-- AUTO_INCREMENT pro tabulku `registrations`
 --
-ALTER TABLE `registrations2`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
+ALTER TABLE `registrations`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=156;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
