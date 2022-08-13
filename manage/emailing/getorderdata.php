be7aa6a5a5aaaf4728770543ad91d9e9bddf3b88
SimonB87 <buryans@gmail.com>
2022-08-13 23:51:15 +0200

    get order data - added otherTicketOptions and otherDancerKind

--- manage/emailing/getorderdata.php
+++ manage/emailing/getorderdata.php
@@ -47,6 +47,7 @@ $errorIcon = "<span><svg xmlns='http://www.w3.org/2000/svg' width='16' height='1
     $storedconfirmPrivateInformation3 = "string";
     $storedOtherTicketOptions = "string";
     $storedDancerKind = "string";
+    $storedOtherDancerKind = "string";
     $storedEventName = "string";
     $storedFormPrice = "string";
     $storedLocation = "string";
@@ -77,6 +78,8 @@ $errorIcon = "<span><svg xmlns='http://www.w3.org/2000/svg' width='16' height='1
         $storedOrderID = $row["orderID"];
         $storedPassType = $row["passType"];
         $storedRegistrationdate = $row["registrationdate"];
+        $storedOtherTicketOptions = $row["otherTicketOptions"];
+        $storedOtherDancerKind = $row["otherDancerKind"];
     
       }
 
