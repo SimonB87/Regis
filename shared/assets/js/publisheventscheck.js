36f4b8af672a0f5cf763ac27ea9770d5f5576b68
SimonB87 <buryans@gmail.com>
2022-08-20 21:00:24 +0200

    js - edited indentation

--- shared/assets/js/publisheventscheck.js
+++ shared/assets/js/publisheventscheck.js
@@ -1,6 +1,6 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function(event) { 
-  
+document.addEventListener("DOMContentLoaded", function (event) {
+
   checkEventStatus();
 
 });
@@ -13,32 +13,32 @@ function checkEventStatus() {
 
 
   for (let i = 0; i < statusSelects.length; i++) {
-    if(statusSelects[i].value == "1 - Registrations open") {
+    if (statusSelects[i].value == "1 - Registrations open") {
       openEvents = openEvents + 1;
     }
-  
-  if (openEvents == 1) {
 
-    statusBackground.classList.remove("bg-success","bg-warning","bg-info");
-    statusBackground.classList.add("bg-success");
-    statusNotification.innerText = "Success - One event is published, all is correct";
-    statusNotification.classList.add("text-white");
+    if (openEvents == 1) {
 
-  } else if (openEvents > 1) {
+      statusBackground.classList.remove("bg-success", "bg-warning", "bg-info");
+      statusBackground.classList.add("bg-success");
+      statusNotification.innerText = "Success - One event is published, all is correct";
+      statusNotification.classList.add("text-white");
 
-    statusBackground.classList.remove("bg-success","bg-warning","bg-info");
-    statusBackground.classList.add("bg-warning");
-    statusNotification.innerText = "Error - There are too many events published. Publis only one event!";
-    statusNotification.classList.remove("text-white");
+    } else if (openEvents > 1) {
 
-  } else if (openEvents == 0) {
+      statusBackground.classList.remove("bg-success", "bg-warning", "bg-info");
+      statusBackground.classList.add("bg-warning");
+      statusNotification.innerText = "Error - There are too many events published. Publis only one event!";
+      statusNotification.classList.remove("text-white");
 
-    statusBackground.classList.remove("bg-success","bg-warning","bg-info");
-    statusBackground.classList.add("bg-info");
-    statusNotification.innerText = "Info - There is no event published, pick an event to publish or create a new one!";
-    statusNotification.classList.add("text-white");
-    
-  }
+    } else if (openEvents == 0) {
+
+      statusBackground.classList.remove("bg-success", "bg-warning", "bg-info");
+      statusBackground.classList.add("bg-info");
+      statusNotification.innerText = "Info - There is no event published, pick an event to publish or create a new one!";
+      statusNotification.classList.add("text-white");
+
+    }
 
   }
 
