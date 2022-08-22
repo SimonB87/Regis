54c45d9cc470a0fb5adf8cbe65cedf9bd5b10290
SimonB87 <buryans@gmail.com>
2022-08-22 21:17:21 +0200

    test environment - notification

---
+++ manage/handlers/handler_testenvironment.php
@@ -0,0 +1,6 @@
+<?php
+
+$urlpath = $_SERVER['REQUEST_URI'];
+$isTestDevelopment = strpos($urlpath, "test"); // createneweventsetupmain // createneweventeventdescription //createneweventeventformpreview // createneweventposter.php
+
+?>
