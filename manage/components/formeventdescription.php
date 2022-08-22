4962911a632696173551ad9817c6aa8e358dd457
SimonB87 <buryans@gmail.com>
2022-08-22 23:57:22 +0200

    event description - code examples

--- manage/components/formeventdescription.php
+++ manage/components/formeventdescription.php
@@ -1,9 +1,53 @@
+<div class="bg-light rounded border col-12">
+  <div class="padding-regular">
+    <h3> Note - formating code </h3>
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Bold text</h4>
+        <!-- <p> <strong>Some bold text here</strong> </p> -->
+
+        <div>
+          < <span class="code-space"> </span> strong <span class="code-space"> </span> > Some bold text here <<span class="code-space"> </span> /<span class="code-space"> </span> strong <span class="code-space"> </span> >
+        </div>
+
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Italic text</h4>
+        <p> 
+          <div>
+          < <span class="code-space"> </span> i <span class="code-space"> </span> > Some italic text here < <span class="code-space"></span>/ <span class="code-space"> </span> i <span class="code-space"> </span> > 
+          </div>
+        </p>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>End of line</h4>
+        <p> Text on line one. < <span class="code-space"> </span> br <span class="code-space"> </span> > Text on line 2. </p>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-12">
+        <h4>Paragraph</h4>
+        <p>
+          <div>
+          < <span class="code-space"> </span> p <span class="code-space"> </span> > Text in par. 1 < <span class="code-space"> </span> / <span class="code-space"> </span> p <span class="code-space"> </span> > <br>
+          < <span class="code-space"> </span> p <span class="code-space"> </span> > Text in par. 2 < <span class="code-space"> </span> / <span class="code-space"> </span> p <span class="code-space"> </span> >
+          </div>
+        </p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
 <div class="col-12">
   <form id="eventdescriptionform" class="needs-validation bts-form eventdescriptionform" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
     <div class="form-floating disabled hidden">
       <input type="number" class="form-control bg-disabled prefill-auto" id="eventID" name="eventID" maxlength="250" value=<?php echo $eventDataId ; ?> >
       <label for="eventID">Event ID (Filled automatically)</label>
     </div>
+
     <div class="form-floating">
       <label for="eventDescription1">Event description part 1</label>
       <textarea class="form-control" id="eventDescription1" name="eventDescription1" rows="10"><?php if($isEventEdited) {echo $dataEventDescription1; } ?></textarea>
