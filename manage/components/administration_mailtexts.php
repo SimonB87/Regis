74938abb7b8882fcc27a143b2967f76340a7e024
SimonB87 <buryans@gmail.com>
2022-08-24 17:35:13 +0200

    Administration - set email texts - setup

--- manage/components/administration_mailtexts.php
+++ manage/components/administration_mailtexts.php
@@ -1,6 +1,126 @@
-<div id="pill-mailtexts-section" class="row padding-regular-top padding-regular-bottom tab-section hidden">
-      <div class="col-12"> 
-        <h3 class="display-7 fw-bold"> Edit texts in emaile </h3> 
-        <div class="text-gray"> Edit information regarding bak transfer for ticket purchase </div> 
+<div id="pill-mailtexts-section" class="row padding-regular-topBottom tab-section hidden">
+
+  <div class="row">
+
+    <div class="col-12"> 
+      <h3 class="display-7 fw-bold"> Edit texts in emaile </h3> 
+      <div class="text-gray padding-regular-bottom"> Edit information regarding bak transfer for ticket purchase </div> 
+    </div>
+
+  </div>
+
+  <div class="accordion" id="accordionEmail">
+    <!-- accordion item 1 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="emailText_headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#emailText_collapseOne" aria-expanded="true" aria-controls="emailText_collapseOne">
+          Email - registration notice
+        </button>
+      </h2>
+      <div id="emailText_collapseOne" class="accordion-collapse collapse show" aria-labelledby="emailText_headingOne" data-bs-parent="#accordionEmail">
+        <div class="accordion-body">
+
+          <div class="row">
+            <!-- accordion item content -->
+            <div class="col-lg-8 col-md-6 col-sm-12">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img />
+            </div>
+            
+          </div>
+
+        </div>
       </div>
+    </div>
+
+    <!-- accordion item 2 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="emailText_headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#emailText_collapseTwo" aria-expanded="false" aria-controls="emailText_collapseTwo">
+          Email - order paid
+        </button>
+      </h2>
+      <div id="emailText_collapseTwo" class="accordion-collapse collapse" aria-labelledby="emailText_headingTwo" data-bs-parent="#accordionEmail">
+        <div class="accordion-body">
+
+          <div class="row">
+            <!-- accordion item content -->
+            <div class="col-lg-8 col-md-6 col-sm-12">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- accordion item 3 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="emailText_headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#emailText_collapseThree" aria-expanded="false" aria-controls="emailText_collapseThree">
+          Email - payment reminder
+        </button>
+      </h2>
+      <div id="emailText_collapseThree" class="accordion-collapse collapse" aria-labelledby="emailText_headingThree" data-bs-parent="#accordionEmail">
+        <div class="accordion-body">
+
+          <div class="row">
+            <!-- accordion item content -->
+            <div class="col-lg-8 col-md-6 col-sm-12">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img />
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- accordion item 4 -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="emailText_headingFour">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#emailText_collapseFour" aria-expanded="false" aria-controls="emailText_collapseFour">
+          Email - registration cancelled
+        </button>
+      </h2>
+      <div id="emailText_collapseFour" class="accordion-collapse collapse" aria-labelledby="emailText_headingFour" data-bs-parent="#accordionEmail">
+        <div class="accordion-body">
+
+          <div class="row">
+            <!-- accordion item content -->
+            <div class="col-lg-8 col-md-6 col-sm-12">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img />
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col-12">
+    <div class="bg-light rounded border margin-small-topBottom">
+      <div class="padding-small">
+        <button class="btn btn-primary btn-lg"> Update </button>
+      </div>
+    </div>
+  </div>
+
 </div>
