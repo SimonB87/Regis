25bb963b0c6f16f7d0714ff01d37dbd70fcd3226
SimonB87 <buryans@gmail.com>
2022-08-24 16:48:49 +0200

    administration - separate navigation

---
+++ manage/components/administration_pagenavigation.php
@@ -0,0 +1,31 @@
+  <div class="container bg-white">
+    <div class="row padding-regular-top">
+      <div class="col-12 rounded bg-light border border-primary padding-regular"> 
+
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a id="pill-appuser" class="nav-link pill-linkitem active" aria-current="page" href="#administration">App users</a>
+          </li>
+          <li class="nav-item">
+            <a id="pill-navregistration" class="nav-link pill-linkitem" href="#administration"> Registration page menu </a>
+          </li>
+          <li class="nav-item">
+            <a id="pill-footerlinks" class="nav-link pill-linkitem" href="#administration"> Links in page footer </a>
+          </li>
+
+          <li class="nav-item">
+            <a id="pill-paymentinfo" class="nav-link pill-linkitem" href="#administration"> Edit main app texts </a>
+          </li>
+
+          <li class="nav-item">
+            <a id="pill-mailtexts" class="nav-link pill-linkitem" href="#administration"> Edit texts in emails </a>
+          </li>
+
+          <li class="nav-item">
+            <a id="pill-fileuploads" class="nav-link pill-linkitem" href="#administration"> File uploads </a>
+          </li>
+        </ul>
+      
+      </div>
+    </div>
+  </div>
