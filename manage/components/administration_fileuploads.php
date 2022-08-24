035c6c4780e2b8979822258ce5bf519e3f983e65
SimonB87 <buryans@gmail.com>
2022-08-24 17:37:40 +0200

    Administration - file upload - added notice

--- manage/components/administration_fileuploads.php
+++ manage/components/administration_fileuploads.php
@@ -1,6 +1,16 @@
 <div id="pill-fileuploads-section" class="row padding-regular-top padding-regular-bottom tab-section hidden">
-      <div class="col-12"> 
-        <h3 class="display-7 fw-bold"> File uploads </h3> 
-        <div class="text-gray"> Check and delete all file in the upload folder </div> 
-      </div>
+  <div class="col-12"> 
+    <h3 class="display-7 fw-bold"> File uploads </h3> 
+    <div class="text-gray"> Check and delete all file in the upload folder </div> 
+  </div>
+
+  <div class="col-12">
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+    TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
+  </div>
+
 </div>
