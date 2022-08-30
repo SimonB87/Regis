7f8707ace0e397f7f354085a39cc4977c13acd30
SimonB87 <buryans@gmail.com>
2022-08-30 19:34:05 +0200

    administration - navigation pills - edited order, disabled to fle management

--- manage/components/administration_pagenavigation.php
+++ manage/components/administration_pagenavigation.php
@@ -3,14 +3,9 @@
       <div class="col-12 rounded bg-light border border-primary padding-regular"> 
 
         <ul class="nav nav-pills">
+
           <li class="nav-item">
             <a id="pill-appuser" class="nav-link pill-linkitem active" aria-current="page" href="#administration">App users</a>
-          </li>
-          <li class="nav-item">
-            <a id="pill-navregistration" class="nav-link pill-linkitem" href="#administration"> Registration page menu </a>
-          </li>
-          <li class="nav-item">
-            <a id="pill-footerlinks" class="nav-link pill-linkitem" href="#administration"> Links in page footer </a>
           </li>
 
           <li class="nav-item">
@@ -18,12 +13,21 @@
           </li>
 
           <li class="nav-item">
+            <a id="pill-navregistration" class="nav-link pill-linkitem" href="#administration"> Registration page menu </a>
+          </li>
+
+          <li class="nav-item">
+            <a id="pill-footerlinks" class="nav-link pill-linkitem" href="#administration"> Links in page footer </a>
+          </li>
+
+          <li class="nav-item">
             <a id="pill-mailtexts" class="nav-link pill-linkitem" href="#administration"> Edit texts in emails </a>
           </li>
 
           <li class="nav-item">
-            <a id="pill-fileuploads" class="nav-link pill-linkitem" href="#administration"> File uploads </a>
+            <a id="pill-fileuploads" class="nav-link pill-linkitem disabled" href="#administration"> Files management </a>
           </li>
+
         </ul>
       
       </div>
